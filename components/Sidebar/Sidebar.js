3fb31d62f19de451aaf0ff90f3eb34e19ff6d82b
ElementalCrisis <richard@richardvanhorn.com>
2016-08-09 02:51:21 -0700

    CSS Updates

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -9,31 +9,31 @@ class Sidebar extends React.Component {
                     <ul className="sidebar-menu" id="nav-accordion">
                         <li>
                             <Link className="active" to="/dashboard">
-                                <i className="fa fa-dashboard"/>
+                                <i className="fa fa-home"/>
                                 <span>Dashboard</span>
                             </Link>
                         </li>
                         <li className="sub-menu">
                             <a href="javascript:;" >
-                                <i className="fa fa-laptop"/>
+                                <i className="fa fa-list-ol"/>
                                 <span>Actions</span>
                             </a>
                         </li>
                         <li className="sub-menu">
                             <a href="javascript:;" >
-                                <i className="fa fa-book"/>
+                                <i className="fa fa-folder"/>
                                 <span>Import Folders</span>
                             </a>
                         </li>
                         <li className="sub-menu">
                             <a href="javascript:;" >
-                                <i className="fa fa-cogs"/>
+                                <i className="fa fa-wrench"/>
                                 <span>Settings</span>
                             </a>
                         </li>
                         <li className="sub-menu">
                             <a href="javascript:;" >
-                                <i className="fa fa-tasks"/>
+                                <i className="fa fa-file-text"/>
                                 <span>Interactive Log</span>
                             </a>
                         </li>
