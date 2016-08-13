fa16adf5a3d9cccabeda996124023bde7cc86d1a
hidden4003 <hidden@4you.lt>
2016-08-13 11:26:57 +0300

    Profile menu fixes (issue #14)

--- components/UserDropdown/UserDropdown.jsx
+++ components/UserDropdown/UserDropdown.jsx
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
 import cx from 'classnames';
 import { Dropdown, MenuItem } from 'react-bootstrap';
-import UserToggle from './UserToggle';
 import s from './UserDropdown.css';
 
 class UserDropdown extends React.Component {
@@ -14,13 +13,13 @@ class UserDropdown extends React.Component {
     return (
       <div className={cx('nav notifications pull-right', s['user-dropdown'])}>
         <Dropdown bsStyle="white" title="User" className="pull-right" id="user">
-          <UserToggle user={user} bsRole="toggle" />
-          <ul className={cx('dropdown-menu', s.menu)} bsRole="menu">
+          <Dropdown.Toggle className={s['user-toggle']} useAnchor>{user}</Dropdown.Toggle>
+          <Dropdown.Menu className={cx('dropdown-menu', s.menu)} bsRole="menu">
             <MenuItem eventKey="1"><i className="fa fa-user" />Profile</MenuItem>
             <MenuItem divider />
             <MenuItem eventKey="2"><i className="fa fa-key" />Change password</MenuItem>
             <MenuItem eventKey="3"><i className="fa fa-sign-out" />Logout</MenuItem>
-          </ul>
+          </Dropdown.Menu>
         </Dropdown>
       </div>
     );
