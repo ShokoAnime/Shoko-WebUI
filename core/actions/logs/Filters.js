226b11cf78b6e7ed365b55fdc3989228abd85c7d
hidden4003 <hidden@4you.lt>
2016-09-27 14:56:31 +0300

    Log filtering (#62)
    * Add filtering by tags and keyword, also some styles
    
    * Bump version to 0.2.0-dev

---
+++ core/actions/logs/Filters.js
@@ -0,0 +1,6 @@
+import { createAction } from 'redux-actions';
+
+export const SET_FILTERS = 'LOGS_SET_FILTERS';
+export const setFilters = createAction(SET_FILTERS);
+export const SET_KEYWORD = 'LOGS_SET_KEYWORD';
+export const setKeyword = createAction(SET_KEYWORD);
---
+++ core/reducers/logs/Filters.js
@@ -0,0 +1,17 @@
+import { handleAction } from 'redux-actions';
+import { SET_FILTERS, SET_KEYWORD } from '../../actions/logs/Filters';
+
+const defaultFilters = {
+  tags: [],
+};
+export const filters = handleAction(SET_FILTERS, (state, action) => {
+  if (action.error) { return state; }
+  return Object.assign({}, state, { tags: action.payload || [] });
+}, defaultFilters);
+
+export const keyword = handleAction(SET_KEYWORD, (state, action) => {
+  if (action.error) { return state; }
+  return action.payload || '';
+}, '');
+
+export default {};
