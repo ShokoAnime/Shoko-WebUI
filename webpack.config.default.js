6a36e657e2617aa0a4abf83c0161a0f0796d0543
hidden4003 <hidden@4you.lt>
2017-05-03 22:01:05 +0300

    Update webpack config

--- webpack.config.default.js
+++ webpack.config.default.js
@@ -9,19 +9,15 @@ const isDebug = global.DEBUG === false ? false : !process.argv.includes('--relea
 const isVerbose = process.argv.includes('--verbose') || process.argv.includes('-v');
 const isBuilding = global.BUILDING;
 const useHMR = !!global.HMR; // Hot Module Replacement (HMR)
-const babelConfig = Object.assign({}, pkg.babel, {
-  babelrc: false,
-  cacheDirectory: useHMR,
-});
 
 const config = {
   context: __dirname,
   entry: [
-    '!!style!css!bootstrap/dist/css/bootstrap.min.css',
-    'font-awesome-webpack',
-    '!!style!css!./css/bootstrap-reset.css',
-    '!!style!css!./css/main.css',
-    './main.jsx',
+    '!!style-loader!css-loader!roboto-npm-webfont/style.css',
+    '!!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css',
+    '!!style-loader!css-loader!./css/bootstrap-reset.css',
+    '!!style-loader!css-loader!./css/main.css',
+    './src/main.jsx',
   ],
   output: {
     path: path.resolve(__dirname, './public/dist'),
@@ -30,11 +26,15 @@ const config = {
     chunkFilename: isDebug ? '[id].js?[chunkhash]' : '[id].[chunkhash].js',
     sourcePrefix: '  ',
   },
-  debug: isDebug,
+  resolveLoader: {
+    alias: {
+      'routes-loader': path.join(__dirname, './utils/routes-loader'),
+    },
+  },
   devServer: {},
   devtool: isDebug ? 'source-map' : false,
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['.js', '.jsx'],
   },
   stats: {
     colors: true,
@@ -48,7 +48,9 @@ const config = {
     cachedAssets: isVerbose,
   },
   plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      debug: true,
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(global.NODE_ENV),
       __DEV__: isDebug,
@@ -62,95 +64,67 @@ const config = {
     }),
   ],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         include: [
-          path.resolve(__dirname, './components'),
-          path.resolve(__dirname, './core'),
-          path.resolve(__dirname, './pages'),
-          path.resolve(__dirname, './main.jsx'),
+          path.resolve(__dirname, './src'),
         ],
-        loader: `babel-loader?${JSON.stringify(babelConfig)}`,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
       },
       {
-        test: /\.css/,
-        loaders: [
+        test: /\.css$/,
+        use: [
           'style-loader',
-          `css-loader?${JSON.stringify({
-            sourceMap: isDebug,
-            modules: true,
-            localIdentName: isDebug ? '[name]_[local]_[hash:base64:3]' : '[hash:base64:4]',
-            minimize: !isDebug,
-          })}`,
-          'postcss-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              sourceMap: isDebug,
+              localIdentName: isDebug ? '[name]_[local]_[hash:base64:3]' : '[hash:base64:4]',
+              minimize: !isDebug,
+            },
+          },
+          { loader: 'postcss-loader' },
         ],
       },
       {
-        test: /\.json$/,
-        exclude: [
-          path.resolve(__dirname, './routes.json'),
-        ],
-        loader: 'json-loader',
-      },
-      {
-        test: /\.json$/,
+        test: /routes\.json$/,
         include: [
-          path.resolve(__dirname, './routes.json'),
+          path.resolve(__dirname, './src/routes.json'),
         ],
-        loaders: [
-          `babel-loader?${JSON.stringify(babelConfig)}`,
-          path.resolve(__dirname, './utils/routes-loader.js'),
+        use: [
+          'babel-loader',
+          'routes-loader',
         ],
-      },
-      { // font-awesome fonts
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      },
-      { // font-awesome fonts
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
       },
       {
-        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-        loader: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(eot|ttf|wav|mp3)$/,
-        loader: 'file-loader',
+        test: /\.(woff|woff2|svg|eot|ttf|wav|mp3)$/,
+        use: [
+          'file-loader',
+        ],
       },
     ],
-  },
-  postcss(bundler) {
-    return [
-      require('postcss-import')({ addDependencyTo: bundler }),
-      require('postcss-custom-properties')(),
-      require('postcss-custom-media')(),
-      require('postcss-media-minmax')(),
-      require('postcss-custom-selectors')(),
-      require('postcss-calc')(),
-      require('postcss-nesting')(),
-      require('postcss-color-function')(),
-      require('pleeease-filters')(),
-      require('pixrem')(),
-      require('postcss-selector-matches')(),
-      require('postcss-selector-not')(),
-      require('autoprefixer')(),
-    ];
   },
 };
 
 if (!isDebug) {
-  config.plugins.push(new webpack.optimize.DedupePlugin());
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: isVerbose } }));
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    mangle: false,
+    compress: { warnings: isVerbose },
+  }));
   config.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
 }
 
 if (isDebug && useHMR) {
-  babelConfig.plugins.unshift('react-hot-loader/babel');
   config.entry.unshift('react-hot-loader/patch', 'webpack-hot-middleware/client');
   config.plugins.push(new webpack.HotModuleReplacementPlugin());
-  config.plugins.push(new webpack.NoErrorsPlugin());
+  config.plugins.push(new webpack.NoEmitOnErrorsPlugin());
 }
 
 module.exports = config;
