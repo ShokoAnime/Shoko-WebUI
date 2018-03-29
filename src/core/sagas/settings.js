5be0fad01a807ab6acd8f4eae0e80a283a95189b
hidden4003 <hidden@4you.lt>
2018-03-29 22:14:25 +0300

    Fix some settings not saving

---
+++ src/core/sagas/settings.js
@@ -0,0 +1,54 @@
+// @flow
+import { call, put, select } from 'redux-saga/effects';
+import Ajv from 'ajv';
+import type { Saga } from 'redux-saga';
+import { QUEUE_GLOBAL_ALERT } from '../actions';
+import Api from '../api';
+import type { Action } from '../actions';
+import type { State } from '../store';
+
+export const settingsSelector = (state: State) => state.settings;
+
+export function* settingsSaveWebui(action: Action): Saga<void> {
+  const settings = yield select(settingsSelector);
+  const currentSettings = {
+    uiTheme: settings.ui.theme,
+    uiNotifications: settings.ui.notifications,
+    otherUpdateChannel: settings.other.updateChannel,
+    logDelta: settings.other.logDelta,
+  };
+  const data = { ...currentSettings, ...action.payload };
+
+  const schema = {
+    required: ['uiTheme', 'uiNotifications', 'otherUpdateChannel', 'logDelta'],
+    uiTheme: { enum: ['light', 'dark', 'custom'] },
+    uiNotifications: { type: 'boolean' },
+    otherUpdateChannel: { enum: ['stable', 'unstable'] },
+    logDelta: { type: 'integer', minimum: 1, maximum: 1000 },
+  };
+  // $FlowFixMe
+  const ajv = new Ajv();
+  const validator = ajv.compile(schema);
+  const result = validator(data);
+  if (result !== true) {
+    yield put({
+      type: QUEUE_GLOBAL_ALERT,
+      payload: { type: 'error', text: `Schema validation failed! ${result.toString()}` },
+    });
+    return;
+  }
+
+  const resultJson = yield call(Api.postWebuiConfig, data);
+  if (resultJson.error) {
+    yield put({ type: QUEUE_GLOBAL_ALERT, payload: { type: 'error', text: resultJson.message } });
+  } else {
+    yield put({
+      type: QUEUE_GLOBAL_ALERT,
+      payload: { type: 'success', text: 'WebUI settings saved!' },
+    });
+  }
+}
+
+export default {
+  saveWebui: settingsSaveWebui,
+};
---
+++ test/sagas/settingsSaveWebui.test.js
@@ -0,0 +1,56 @@
+import { select, call, put } from 'redux-saga/effects';
+import { cloneableGenerator } from 'redux-saga/utils';
+import proxyquire from 'proxyquire';
+import test from 'ava';
+import { createStore } from 'redux';
+import reducer from '../../src/core/reducers/settings';
+import { QUEUE_GLOBAL_ALERT } from '../../src/core/actions';
+
+const action = {
+  payload: {
+    otherUpdateChannel: 'stable',
+    logDelta: 0,
+  },
+};
+
+const state = createStore(reducer).getState();
+
+const mockApi = {
+  postWebuiConfig: () => ({}),
+  '@noCallThru': true,
+};
+
+const settingsSagas = proxyquire('../../src/core/sagas/settings', {
+  '../api': mockApi,
+});
+
+test('settingsSaveWebui', (t) => {
+  const generator = cloneableGenerator(settingsSagas.settingsSaveWebui)(action);
+  const effectSelect = select(settingsSagas.settingsSelector);
+  t.deepEqual(effectSelect, generator.next().value, 'select effect with settings selector');
+
+  const currentSettings = {
+    uiTheme: state.ui.theme,
+    uiNotifications: state.ui.notifications,
+    otherUpdateChannel: state.other.updateChannel,
+    logDelta: state.other.logDelta,
+  };
+  const data = { ...currentSettings, ...action.payload };
+  const effectCall = call(mockApi.postWebuiConfig, data);
+  t.deepEqual(effectCall, generator.next(state).value, 'call effect with Api.postWebuiConfig');
+
+  const cloneError = generator.clone();
+  const responseError = { error: true, message: 'Test error' };
+  const effectPutError = put({ type: QUEUE_GLOBAL_ALERT, payload: { type: 'error', text: responseError.message } });
+  t.deepEqual(effectPutError, cloneError.next(responseError).value, 'put effect with error message');
+  t.true(cloneError.next().done, 'should be done');
+
+  const cloneSuccess = generator.clone();
+  const responseSuccess = { data: 'Test data' };
+  const effectSuccess = put({
+    type: QUEUE_GLOBAL_ALERT,
+    payload: { type: 'success', text: 'WebUI settings saved!' },
+  });
+  t.deepEqual(effectSuccess, cloneSuccess.next(responseSuccess).value, 'put effect with success message');
+  t.true(cloneSuccess.next().done, 'should be done');
+});
