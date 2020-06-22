c59b9300ec5932a406930f2656848ea47c9c6449
Harshith Mohan <gharshitmohan@ymail.com>
2020-06-22 22:24:19 +0530

    Add new APIv3 action endpoints and reorder them

--- src/core/api/v3/actions.ts
+++ src/core/api/v3/actions.ts
@@ -4,15 +4,15 @@ function ApiRequest(action: string, expectEmpty = true) {
   return Api.call({ action: `/v3/Action/${action}`, expectEmpty });
 }
 
+// Run Import. This checks for new files, hashes them etc, scans Drop Folders, checks and scans
+// for community site links (tvdb, trakt, moviedb, etc), and downloads missing images.
+function getRunImport() {
+  return ApiRequest('RunImport');
+}
+
 // This was for web cache hash syncing, and will be for perceptual hashing maybe eventually.
 function getSyncHashes() {
   return ApiRequest('SyncHashes');
-}
-
-// BEWARE this is a dangerous command! It syncs all of the states in Shoko's library to AniDB.
-// ONE WAY. THIS CAN ERASE ANIDB DATA IRREVERSIBLY
-function getSyncMyList() {
-  return ApiRequest('SyncMyList');
 }
 
 // Sync the votes from Shoko to AniDB.
@@ -30,16 +30,6 @@ function getRemoveMissingFiles(removeFromMyList: boolean) {
   return ApiRequest(`RemoveMissingFiles/${removeFromMyList}`);
 }
 
-// Update All AniDB Series Info
-function getUpdateAllAniDBInfo() {
-  return ApiRequest('UpdateAllAniDBInfo');
-}
-
-// Queues a task to Update all media info
-function getUpdateAllMediaInfo() {
-  return ApiRequest('UpdateAllMediaInfo');
-}
-
 // Update All TvDB Series Info
 function getUpdateAllTvDBInfo() {
   return ApiRequest('UpdateAllTvDBInfo');
@@ -50,14 +40,14 @@ function getUpdateAllImages() {
   return ApiRequest('UpdateAllImages');
 }
 
+// Updates All MovieDB Info
+function getUpdateAllMovieDBInfo() {
+  return ApiRequest('UpdateAllMovieDBInfo');
+}
+
 // Update all Trakt info. Right now, that's not much.
 function getUpdateAllTraktInfo() {
   return ApiRequest('UpdateAllTraktInfo');
-}
-
-// Queues commands to Update All Series Stats and Force a Recalculation of All Group Filters
-function getUpdateSeriesStats() {
-  return ApiRequest('UpdateSeriesStats');
 }
 
 // Validates all images
@@ -65,17 +55,63 @@ function getValidateAllImages() {
   return ApiRequest('ValidateAllImages');
 }
 
+// Gets files whose data does not match AniDB
+function getAVDumpMisMatchedFiles() {
+  return ApiRequest('AVDumpMismatchedFiles');
+}
+
+// This Downloads XML data from AniDB where there is none. This should only happen:
+// A. If someone deleted or corrupted them.
+// B. If the server closed unexpectedly at the wrong time (it'll only be one).
+// C. If there was a catastrophic error.
+function getDownloadMissingAniDBAnimeData() {
+  return ApiRequest('DownloadMissingAniDBAnimeData');
+}
+
+// Regenerate All Episode Matchings for TvDB. Generally, don't do this unless there was an error
+// that was fixed. In those cases, you'd be told to.
+function getRegenerateAllTvDBEpisodeMatchings() {
+  return ApiRequest('RegenerateAllTvDBEpisodeMatchings');
+}
+
+// BEWARE this is a dangerous command! It syncs all of the states in Shoko's library to AniDB.
+// ONE WAY. THIS CAN ERASE ANIDB DATA IRREVERSIBLY
+function getSyncMyList() {
+  return ApiRequest('SyncMyList');
+}
+
+// Update All AniDB Series Info
+function getUpdateAllAniDBInfo() {
+  return ApiRequest('UpdateAllAniDBInfo');
+}
+
+// Queues a task to Update all media info
+function getUpdateAllMediaInfo() {
+  return ApiRequest('UpdateAllMediaInfo');
+}
+
+// Queues commands to Update All Series Stats and Force a Recalculation of All Group Filters
+function getUpdateSeriesStats() {
+  return ApiRequest('UpdateSeriesStats');
+}
+
+
 export default {
+  getRunImport,
   getSyncHashes,
-  getSyncMyList,
   getSyncVotes,
   getSyncTrakt,
   getRemoveMissingFiles,
-  getUpdateAllAniDBInfo,
-  getUpdateAllMediaInfo,
   getUpdateAllTvDBInfo,
   getUpdateAllImages,
+  getUpdateAllMovieDBInfo,
   getUpdateAllTraktInfo,
-  getUpdateSeriesStats,
   getValidateAllImages,
+  getAVDumpMisMatchedFiles,
+  getDownloadMissingAniDBAnimeData,
+  getRegenerateAllTvDBEpisodeMatchings,
+  getSyncMyList,
+  getUpdateAllAniDBInfo,
+  getUpdateAllMediaInfo,
+  getUpdateSeriesStats,
 };
