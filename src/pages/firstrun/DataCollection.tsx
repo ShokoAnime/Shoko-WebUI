e8f2f6c2cfd689ebb4cf574899589351c3f90629
G Harshith Mohan <gharshitmohan@ymail.com>
2020-08-02 23:57:12 +0530

    Add content to ack and data collection page of first run

--- src/pages/firstrun/DataCollection.tsx
+++ src/pages/firstrun/DataCollection.tsx
@@ -9,7 +9,7 @@ type State = {
 
 class DataCollection extends React.Component<{}, State> {
   state = {
-    collectData: false,
+    collectData: true,
   };
 
   handleInputChange = (event: any) => {
@@ -22,17 +22,51 @@ class DataCollection extends React.Component<{}, State> {
     const { collectData } = this.state;
     return (
       <React.Fragment>
-        <div className="flex flex-col flex-grow p-10">
+        <div className="flex flex-col flex-grow pt-10 px-10 pb-5 overflow-y-auto">
           <div className="font-bold text-lg">Data Collection</div>
-          <div className="font-muli mt-5">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
+          <div className="overflow-y-auto">
+            <div className="font-muli mt-5">
+              Shoko is open-source and is worked on by volunteers in our free time so in order to
+              more effectively improve Shoko, we've implemented two different services for data
+              collection. We understand the hesitations a lot of people have with data collection
+              services so we're listing what services we use and what data we collect.
+            </div>
+            <div className="font-muli mt-4">
+              We also want to make something very clear, <span className="font-bold">all data
+              collected is non-identifiable.</span>We don’t care about what specific titles you have
+              in your collection or what you watch. We only care about making Shoko better which is
+              what this data will allow us to do. 
+            </div>
+            <div className="font-muli mt-4">
+              <span className="font-bold">Sentry</span>
+              <ul>
+                <li>- Error information and context</li>
+              </ul>
+            </div>
+            <div className="font-muli mt-4">
+              <span className="font-bold">Google Analytics</span>
+              <ul>
+                <li>- AniDB - Ban Type (HTTP/UDP)</li>
+                <li>- Import - Run Import</li>
+                <li>- Import - Run Import (Scan TVDB)</li>
+                <li>- Import - Run Import (Scan MovieDB)</li>
+                <li>- Import - Run Import (Update TVDB)</li>
+                <li>- Import - Run Import (Update All AniDB)</li>
+                <li>- Import - Run Import (Update All Stats)</li>
+                <li>- Plex - Manually Syncing Plex (Single User)</li>
+                <li>- Plex - Token Created</li>
+                <li>- Plex - Sync All</li>
+                <li>- Server - Startup Status</li>
+                <li>- Server - Linux Users</li>
+                <li>- Server - Startup Complete</li>
+                <li>- Server - Server Shutdown</li>
+                <li>- Trakt - Scan for Matches</li>
+                <li>- Utility - Removing Missing Files</li>
+                <li>- Utility - Validate All Images</li>
+              </ul>
+            </div>
           </div>
-          <div className="flex items-center mt-12">
+          <div className="flex items-center mt-6">
             <span className="flex mr-6">Allow Shoko To Collect Anonymous Data</span>
             <span className="flex">
               <Checkbox id="collectData" isChecked={collectData} onChange={this.handleInputChange} />
