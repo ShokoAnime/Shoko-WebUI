df8a2c39c7576c3e18095a7e688dc327e95fe8ca
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2022-07-13 01:34:43 -0700

    Run ESLint on file.

--- src/components/ShokoIcon.tsx
+++ src/components/ShokoIcon.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+import * as React from 'react';
 
-const SVGComponent = (props) => (
+const SVGComponent = props => (
   <svg
     className="shoko-icon"
     xmlns="http://www.w3.org/2000/svg"
