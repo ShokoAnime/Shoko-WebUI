6d25c4bd44a1f2424d76c442a47523bc9583d60f
Mikal S <revam@users.noreply.github.com>
2022-07-24 07:31:22 +0200

    Fix api (#348)
    * cleanup: remove unused recent file details code
    
    * refactor: fix breakage in api

--- src/core/types/api/dashboard.ts
+++ src/core/types/api/dashboard.ts
@@ -2,30 +2,30 @@ import { ImageType } from './common';
 import { EpisodeTypeEnum } from './episode';
 
 export type DashboardSeriesSummaryType = {
-  Series?: number,
-  OVA?: number,
-  Movie?: number,
-  Special?: number,
-  Web?: number,
-  Other?: number,
-  None?: number,
+  Series: number,
+  OVA: number,
+  Movie: number,
+  Special: number,
+  Web: number,
+  Other: number,
+  None: number,
 };
 
 export type DashboardStatsType = {
-  FileCount?: number,
-  SeriesCount?: number,
-  GroupCount?: number,
-  FileSize?: number,
-  FinishedSeries?: number,
-  WatchedEpisodes?: number,
-  WatchedHours?: number,
-  PercentDuplicate?: number,
-  MissingEpisodes?: number,
-  MissingEpisodesCollecting?: number,
-  UnrecognizedFiles?: number,
-  SeriesWithMissingLinks?: number,
-  EpisodesWithMultipleFiles?: number,
-  FilesWithDuplicateLocations?: number,
+  FileCount: number,
+  SeriesCount: number,
+  GroupCount: number,
+  FileSize: number,
+  FinishedSeries: number,
+  WatchedEpisodes: number,
+  WatchedHours: number,
+  PercentDuplicate: number,
+  MissingEpisodes: number,
+  MissingEpisodesCollecting: number,
+  UnrecognizedFiles: number,
+  SeriesWithMissingLinks: number,
+  EpisodesWithMultipleFiles: number,
+  FilesWithDuplicateLocations: number,
 };
 
 export type DashboardEpisodeDetailsType = {
