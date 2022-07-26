af3dac6d1ad6885f09dfc095430f54f403460c06
hidden4003 <hidden4003@users.noreply.github.com>
2022-07-27 00:04:10 +0300

    make reload work with router v6

--- webpack.config.default.js
+++ webpack.config.default.js
@@ -5,6 +5,7 @@ const webpack = require('webpack');
 const AssetsPlugin = require('assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const Version = require('./public/version.json');
 
@@ -18,6 +19,7 @@ const config = {
   entry: [
     '@fontsource/open-sans/latin.css',
     './css/main.scss',
+    isDebug && '@pmmmwh/react-refresh-webpack-plugin/client/ReactRefreshEntry.js',
     isDebug ? './src/main-hmr.tsx' : './src/main.tsx',
   ],
   mode: isDebug ? 'development' : 'production',
@@ -49,6 +51,7 @@ const config = {
     optimizationBailout: isVerbose,
   },
   plugins: [
+    new ReactRefreshPlugin(),
     new webpack.LoaderOptionsPlugin({
       debug: isDebug,
     }),
