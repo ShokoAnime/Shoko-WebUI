e665e1f5664c90164b7d4b5249ca66f1df898867
harshithmohan <gharshitmohan@ymail.com>
2022-08-05 17:03:56 +0530

    Fix eslint

--- src/pages/dashboard/panels/QueueProcessor.tsx
+++ src/pages/dashboard/panels/QueueProcessor.tsx
@@ -8,7 +8,7 @@ import {
   mdiImageMultipleOutline,
   mdiPauseBoxOutline,
   mdiPlayBoxOutline,
-  mdiCloseBoxOutline
+  mdiCloseBoxOutline,
 } from '@mdi/js';
 
 import { RootState } from '../../../core/store';
