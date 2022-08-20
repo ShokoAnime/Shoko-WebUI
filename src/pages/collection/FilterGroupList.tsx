619ab873d093df4218dbeef22ce98da54e02c661
hidden4003 <hidden4003@users.noreply.github.com>
2022-08-20 23:05:53 +0300

    change poster sizes (#395)
    clamp titles to 1 line in grid view

--- src/pages/collection/FilterGroupList.tsx
+++ src/pages/collection/FilterGroupList.tsx
@@ -42,7 +42,7 @@ const HoverIcon = ({ icon, label, route }) => (
 
 function FilterGroupList() {
   const itemWidth = 240; //224 + 16
-  const itemHeight = 344; //328 + 16
+  const itemHeight = 374; //328 + 16
   const itemHeightList = 224; //176 + 16
   const pageSize = 50;
   const fetchedPages = useSelector((state: RootState) => state.collection.fetchedPages);
@@ -93,14 +93,14 @@ function FilterGroupList() {
     const posters = item.Images.Posters;
     
     return (
-      <div key={`group-${item.IDs.ID}`} className="group mr-4 last:mr-0 shrink-0 w-56 font-open-sans content-center flex flex-col">
-        <div style={{ background: `center / cover no-repeat url('/api/v3/Image/${posters[0].Source}/Poster/${posters[0].ID}')` }} className="h-72 rounded drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] border border-black my-2">
+      <div key={`group-${item.IDs.ID}`} className="group mr-4 last:mr-0 shrink-0 w-[13.875rem] font-open-sans content-center flex flex-col">
+        <div style={{ background: `center / cover no-repeat url('/api/v3/Image/${posters[0].Source}/Poster/${posters[0].ID}')` }} className="h-[19.875rem] rounded drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] border border-black my-2">
           <div className="hidden group-hover:flex bg-background-nav/85 h-full flex-col justify-center items-center">
             <HoverIcon icon={mdiEyeArrowRightOutline} label="View Group" route={`group/${item.IDs.ID}`} />
             <HoverIcon icon={mdiSquareEditOutline} label="Edit Group" route="" />
           </div>
         </div>
-        <p className="text-center text-base font-semibold" title={item.Name}>{item.Name}</p>
+        <p className="text-center text-base font-semibold text-ellipsis line-clamp-1" title={item.Name}>{item.Name}</p>
       </div>
     );
   };
