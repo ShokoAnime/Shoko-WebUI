5aee68fcd0afcf623ebde447e3f85347d5266239
hidden4003 <hidden4003@users.noreply.github.com>
2022-09-01 23:30:41 +0300

    add files grouping (#404)

--- src/pages/utilities/UnrecognizedUtilityTabs/Components/SelectedFilesPanel.tsx
+++ src/pages/utilities/UnrecognizedUtilityTabs/Components/SelectedFilesPanel.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { forEach } from 'lodash';
+import { find, forEach, groupBy } from 'lodash';
 import cx from 'classnames';
 
 import ShokoPanel from '../../../../components/Panels/ShokoPanel';
@@ -8,7 +8,24 @@ import ShokoPanel from '../../../../components/Panels/ShokoPanel';
 import { RootState } from '../../../../core/store';
 
 function SelectedFilesPanel() {
-  const { selectedSeries, selectedRows } = useSelector((state: RootState) => state.utilities.unrecognized );
+  const { selectedSeries, selectedRows, links } = useSelector((state: RootState) => state.utilities.unrecognized );
+
+  const groupedLinks = useMemo(() => groupBy(links, 'EpisodeID'), [links]);
+
+  const renderFileLinks = () => {
+    const result: React.ReactNode[] = [];
+    forEach(groupedLinks, (episodeLinks) => {
+      forEach(episodeLinks, (link, idx) => {
+        const file = find(selectedRows, ['ID', link.FileID]);
+        result.push(
+          <div className={cx(['px-3 py-3.5 w-full bg-background-nav border border-background-border rounded-md', selectedSeries?.ID && 'mt-4'])} key={`${link.FileID}-${link.EpisodeID}-${idx}`}>
+            {file?.Locations[0].RelativePath}
+          </div>,
+        );
+      });
+    });
+    return result;
+  };
 
   const manualLinkFileRows: Array<React.ReactNode> = [];
   forEach(selectedRows, (file) => {
@@ -22,7 +39,7 @@ function SelectedFilesPanel() {
   return (
     <ShokoPanel title="Selected Files" className="w-1/2">
       <div className="grow basis-0 overflow-y-auto">
-        {manualLinkFileRows}
+        {selectedSeries?.ID ? renderFileLinks() : manualLinkFileRows}
       </div>
     </ShokoPanel>
   );
