fbebbc31654e8b907abde3c6c8960298f927e568
harshithmohan <gharshitmohan@ymail.com>
2022-10-31 00:36:04 +0530

    Add validation to relation depth in settings

--- src/pages/settings/tabs/AniDBSettings.tsx
+++ src/pages/settings/tabs/AniDBSettings.tsx
@@ -77,6 +77,11 @@ function AniDBSettings() {
     }
   };
 
+  const validateAndSaveRelationDepth = (depth: string) => {
+    if (parseInt(depth) < 0 || parseInt(depth) > 5) toast.error('Max Relation Depth may only be between 0 and 5');
+    else updateSetting('AniDb', 'MaxRelationDepth', depth);
+  };
+
   return (
     <>
       <ShokoPanel
@@ -116,7 +121,7 @@ function AniDBSettings() {
         <Checkbox justify label="Related Anime" id="related-anime" isChecked={DownloadRelatedAnime} onChange={event => updateSetting('AniDb', 'DownloadRelatedAnime', event.target.checked)} className="mt-2" />
         <div className={cx('flex justify-between mt-2 items-center transition-opacity', !DownloadRelatedAnime && 'pointer-events-none opacity-50')}>
           Related Depth
-          <InputSmall id="max-relation-depth" value={MaxRelationDepth} type="number" onChange={event => updateSetting('AniDb', 'MaxRelationDepth', event.target.value)} className="w-10 text-center px-2" />
+          <InputSmall id="max-relation-depth" value={MaxRelationDepth} type="number" onChange={event => validateAndSaveRelationDepth(event.target.value)} className="w-10 text-center px-2" />
         </div>
       </ShokoPanel>
 
