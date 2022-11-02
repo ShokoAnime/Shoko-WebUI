e37fe9bbc30d9852ffddd3fc3a1f1714d168c77f
harshithmohan <gharshitmohan@ymail.com>
2022-11-02 12:01:16 +0530

    Update import folder dashboard panel to new design

--- src/pages/dashboard/panels/ImportFolders.tsx
+++ src/pages/dashboard/panels/ImportFolders.tsx
@@ -34,38 +34,41 @@ function ImportFolders() {
     let flags = '';
     switch (folder.DropFolderType) {
       case 1:
-        flags = 'Drop Source';
+        flags = 'Source';
         break;
       case 2:
-        flags = 'Drop Destination';
+        flags = 'Destination';
         break;
       case 3:
-        flags = 'Both Drop Source and Destination';
+        flags = 'Source, Destination';
         break;
       default:
     }
-    if (folder.WatchForNewFiles) flags += folder.DropFolderType ? ', Watch For New Files' : 'Watch For New Files';
+    if (folder.WatchForNewFiles) flags += folder.DropFolderType ? ', Watch' : 'Watch';
 
     return (
       <div key={folder.ID} className="flex flex-col mt-3 first:mt-0">
-        <div className="flex justify-between">
-          <span className="font-semibold">{flags}</span>
-          <span className="text-highlight-2 font-semibold">Online</span>
+        <div className="flex justify-between items-center">
+          <span className="font-semibold">{folder.Name}</span>
+          <div className="flex">
+            <Button onClick={() => rescanFolder(folder.ID, folder.Name)} tooltip="Rescan Folder" className="mr-3">
+              <Icon className="text-highlight-1" path={mdiDatabaseSearchOutline} size={1} horizontal vertical rotate={180}/>
+            </Button>
+            <Button onClick={() => openImportFolderModalEdit(folder.ID)} tooltip="Edit Folder">
+              <Icon className="text-highlight-1" path={mdiDatabaseEditOutline} size={1} horizontal vertical rotate={180}/>
+            </Button>
+          </div>
         </div>
         <div className="mt-2">
-          {folder.Path}
+          Location: {folder.Path}
         </div>
-        <div className="mt-1">
-          Series: {folder.Size ?? 0} / Size: {prettyBytes(folder.FileSize ?? 0, { binary: true })}
+        <div>
+          Type: {flags}
         </div>
-        <div className="mt-1 -ml-[0.313rem] flex">
-          <Button onClick={() => rescanFolder(folder.ID, folder.Name)} tooltip="Rescan Folder">
-            <Icon className="text-highlight-1" path={mdiDatabaseSearchOutline} size={1} horizontal vertical rotate={180}/>
-          </Button>
-          <Button onClick={() => openImportFolderModalEdit(folder.ID)} tooltip="Edit Folder">
-            <Icon className="text-highlight-1" path={mdiDatabaseEditOutline} size={1} horizontal vertical rotate={180}/>
-          </Button>
+        <div>
+          Size: {prettyBytes(folder.FileSize ?? 0, { binary: true })} ({(folder.Size ?? 2043).toLocaleString('en-US')} Series)
         </div>
+
       </div>
     );
   };
