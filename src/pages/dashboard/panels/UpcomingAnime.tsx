04d64b6a4b2c3a570cad92066bc0bbb1e359daa0
harshithmohan <gharshitmohan@ymail.com>
2022-11-11 00:24:25 +0530

    Fix upcoming anime panel

--- src/pages/dashboard/panels/UpcomingAnime.tsx
+++ src/pages/dashboard/panels/UpcomingAnime.tsx
@@ -18,23 +18,18 @@ const UpcomingAnime = () => {
 
   return (
     <ShokoPanel isFetching={items.isFetching} title={<DashboardTitleToggle title="Upcoming Anime" mainTitle="My Collection" secondaryTitle="All" secondaryActive={showAll} setSecondaryActive={setShowAll} />} editMode={layoutEditMode}>
-      <div className="flex flex-nowrap overflow-x-auto shoko-scrollbar h-90 pb-5">
-        {items.data?.length === 0 && <div className="flex justify-center font-semibold mt-4">It looks like you are not watching anything currently airing.</div>}
-        {items.data?.map(item => <EpisodeDetails episode={item} showDate key={item.IDs.ID} />)}
-
-        <TransitionDiv show={!showAll} className="flex flex-nowrap overflow-x-auto shoko-scrollbar pb-5 absolute w-full">
-          {(localItems.data?.length ?? 0) > 0
-            ? localItems.data?.map(item => <EpisodeDetails episode={item} key={item.IDs.ID} />)
-            : <div className="flex justify-center font-semibold mt-4 w-full">No upcoming anime!</div>
-          }
-        </TransitionDiv>
-        <TransitionDiv show={showAll} className="flex flex-nowrap overflow-x-auto shoko-scrollbar pb-5 absolute w-full">
-          {(items.data?.length ?? 0) > 0
-            ? items.data?.map(item =><EpisodeDetails episode={item} showDate key={item.IDs.ID} />)
-            : <div className="flex justify-center font-semibold mt-4 w-full">No upcoming anime!</div>
-          }
-        </TransitionDiv>
-      </div>
+      <TransitionDiv show={!showAll} className="flex flex-nowrap overflow-x-auto shoko-scrollbar pb-5 absolute w-full">
+        {(localItems.data?.length ?? 0) > 0
+          ? localItems.data?.map(item => <EpisodeDetails episode={item} key={item.IDs.ID} />)
+          : <div className="flex justify-center font-semibold mt-4 w-full">No upcoming anime!</div>
+        }
+      </TransitionDiv>
+      <TransitionDiv show={showAll} className="flex flex-nowrap overflow-x-auto shoko-scrollbar pb-5 absolute w-full">
+        {(items.data?.length ?? 0) > 0
+          ? items.data?.map(item =><EpisodeDetails episode={item} showDate key={item.IDs.ID} />)
+          : <div className="flex justify-center font-semibold mt-4 w-full">No upcoming anime!</div>
+        }
+      </TransitionDiv>
     </ShokoPanel>
   );
 };
