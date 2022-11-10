19852f50f820e597e6342cdc1a00d0a72cba3c3e
harshithmohan <gharshitmohan@ymail.com>
2022-11-10 23:15:21 +0530

    Add transitions and empty message to "Recently Imported" panel

--- src/pages/dashboard/panels/RecentlyImported.tsx
+++ src/pages/dashboard/panels/RecentlyImported.tsx
@@ -11,6 +11,7 @@ import {
   useGetDashboardRecentlyAddedEpisodesQuery,
   useGetDashboardRecentlyAddedSeriesQuery,
 } from '../../../core/rtkQuery/dashboardApi';
+import TransitionDiv from '../../../components/TransitionDiv';
 
 const RecentlyImported = () => {
   const layoutEditMode = useSelector((state: RootState) => state.mainpage.layoutEditMode);
@@ -21,11 +22,12 @@ const RecentlyImported = () => {
 
   return (
     <ShokoPanel title={<DashboardTitleToggle title="Recently Imported" mainTitle="Episodes" secondaryTitle="Series" secondaryActive={showSeries} setSecondaryActive={setShowSeries} />} editMode={layoutEditMode}>
-      <div className="flex flex-nowrap overflow-x-auto shoko-scrollbar h-90 pb-5">{showSeries ? (
-        series.data?.map(item => <SeriesDetails series={item} key={item.IDs.ID} />)
-      ) : (
-        episodes.data?.map(item => <EpisodeDetails episode={item} key={item.IDs.ID} />)
-      )}</div>
+      <TransitionDiv show={!showSeries} className="flex flex-nowrap overflow-x-auto shoko-scrollbar pb-5 absolute w-full">
+        {(episodes.data?.length ?? 0) > 0 ? episodes.data?.map(item => <EpisodeDetails episode={item} key={item.IDs.ID} />) : <div className="flex justify-center font-semibold mt-4 w-full">No recently imported episodes!</div>}
+      </TransitionDiv>
+      <TransitionDiv show={showSeries} className="flex flex-nowrap overflow-x-auto shoko-scrollbar pb-5 absolute w-full">
+        {(series.data?.length ?? 0) > 0 ? series.data?.map(item => <SeriesDetails series={item} key={item.IDs.ID} />) : <div className="flex justify-center font-semibold mt-4 w-full">No recently imported series!</div>}
+      </TransitionDiv>
     </ShokoPanel>
   );
 };
