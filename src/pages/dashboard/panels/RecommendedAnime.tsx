d77a44cdfc136894c611640bd21a8c1c73f114fb
harshithmohan <gharshitmohan@ymail.com>
2022-11-14 17:28:02 +0530

    Add AniDB page link to recommended anime

--- src/pages/dashboard/panels/RecommendedAnime.tsx
+++ src/pages/dashboard/panels/RecommendedAnime.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Icon } from '@mdi/react';
+import { mdiEyeArrowRightOutline } from '@mdi/js';
 
 import { RootState } from '../../../core/store';
 import ShokoPanel from '../../../components/Panels/ShokoPanel';
@@ -14,7 +16,17 @@ const RecommendedAnime = () => {
 
   const renderItem = (series: SeriesAniDBType, matches: number) => (
     <div key={`series-${series.ID}`} className="mr-5 last:mr-0 shrink-0 w-56 font-open-sans justify-center flex flex-col">
-      <div style={{ background: `center / cover no-repeat url('/api/v3/Image/AniDB/Poster/${series.Poster.ID}')` }} className="relative h-80 rounded overflow-hidden drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] border border-black mb-2" />
+      <div style={{ background: `center / cover no-repeat url('/api/v3/Image/AniDB/Poster/${series.Poster.ID}')` }} className="relative h-80 rounded overflow-hidden drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] border border-black mb-2">
+        <div
+          className="flex flex-col items-center justify-center w-full h-full font-semibold text-sm cursor-pointer bg-background-nav/85 transition-opacity opacity-0 hover:opacity-100"
+          onClick={() => window.open(`https://anidb.net/anime/${series.ID}`, '_blank')}
+        >
+          <div className="p-5 bg-background-border rounded-full mb-1">
+            <Icon path={mdiEyeArrowRightOutline} size={1} className="text-highlight-1" />
+          </div>
+          View Series on AniDB
+        </div>
+      </div>
       <p className="truncate text-center text-base font-semibold" title={series.Title}>{series.Title}</p>
       <p className="truncate text-center text-sm" title={`${matches} Matches`}>{matches} Matches</p>
     </div>
