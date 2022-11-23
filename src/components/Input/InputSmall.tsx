03a6aa67aba4ce5c478402f345511756f36fa482
harshithmohan <gharshitmohan@ymail.com>
2022-11-24 01:30:26 +0530

    Remove deprecated attribute from InputSmall and add inset ring for focus

--- src/components/Input/InputSmall.tsx
+++ src/components/Input/InputSmall.tsx
@@ -6,7 +6,7 @@ type Props = {
   placeholder?: string;
   value: string | number;
   onChange: (event: any) => void;
-  onKeyPress?: (event: any) => void;
+  onKeyUp?: (event: any) => void;
   className?: string;
   autoFocus?: boolean;
   disabled?: boolean;
@@ -16,19 +16,19 @@ type Props = {
 function InputSmall(props: Props) {
   const {
     id, type, placeholder, value, className,
-    autoFocus, disabled, onChange, onKeyPress,
+    autoFocus, disabled, onChange, onKeyUp,
     autoComplete,
   } = props;
 
   return (
     <input
-      className={`${className} appearance-none bg-background-alt focus:shadow-none focus:outline-none rounded-md text-sm input-field transition ease-in-out border border-background-border focus:border-highlight-1`}
+      className={`${className} appearance-none bg-background-alt focus:shadow-none focus:outline-none rounded-md text-sm input-field transition ease-in-out border border-background-border focus:ring-2 focus:ring-highlight-1 focus:ring-inset`}
       id={id}
       type={type}
       placeholder={placeholder ?? ''}
       value={value}
       onChange={onChange}
-      onKeyPress={onKeyPress}
+      onKeyUp={onKeyUp}
       // eslint-disable-next-line jsx-a11y/no-autofocus
       autoFocus={autoFocus}
       disabled={disabled}
