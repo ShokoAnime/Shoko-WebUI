35da2477d6d89e955292c558f6d25e833cbcc1a6
Harshith Mohan <gharshitmohan@ymail.com>
2022-11-25 13:57:48 +0530

    Change how channel is displayed on login panel

--- src/pages/firstrun/FirstRunPage.tsx
+++ src/pages/firstrun/FirstRunPage.tsx
@@ -80,7 +80,7 @@ function FirstRunPage() {
             Version: {version.isFetching || !version.data ?
               <Icon path={mdiLoading} spin size={1} className="ml-2 text-highlight-1" /> :
             version.data.Server.ReleaseChannel !== 'Stable' ?
-              `${version.data.Server.ReleaseChannel} ${version.data.Server.Version} (${version.data.Server.Commit?.slice(0, 7)})` :
+              `${version.data.Server.Version}-${version.data.Server.ReleaseChannel} (${version.data.Server.Commit?.slice(0, 7)})` :
               version.data.Server.Version
             }
           </div>
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -73,7 +73,7 @@ function LoginPage() {
             Version: {version.isFetching || !version.data ?
               <Icon path={mdiLoading} spin size={1} className="ml-2 text-highlight-1" /> :
             version.data.Server.ReleaseChannel !== 'Stable' ?
-              `${version.data.Server.ReleaseChannel} ${version.data.Server.Version} (${version.data.Server.Commit?.slice(0, 7)})` :
+              `${version.data.Server.Version}-${version.data.Server.ReleaseChannel} (${version.data.Server.Commit?.slice(0, 7)})` :
               version.data.Server.Version
             }
           </div>
