1dc86f0b1da722b2a5e016004b819d44bb1691a5
hidden4003 <hidden4003@users.noreply.github.com>
2022-11-25 22:09:17 +0200

    add toggle to disable scroll to bottom

--- src/pages/logs/LogsPage.tsx
+++ src/pages/logs/LogsPage.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   List, AutoSizer, CellMeasurer, CellMeasurerCache,
 } from 'react-virtualized';
+import cx from 'classnames';
 
+import Checkbox from '../../components/Input/Checkbox';
 import { useGetLogsQuery } from '../../core/rtkQuery/logsApi';
+
 import { LogLineType } from '../../core/types/api/common';
+
 
 function LogsPage() {
   const linesQuery = useGetLogsQuery();
   const lines: LogLineType[] = linesQuery?.data ?? [];
+  const listRef = useRef<List>(null);
+  const [isScrollToBottom, setScrollToBottom] = useState(true);
 
+  useEffect(() => {
+    if (!isScrollToBottom) { return; }
+    listRef?.current?.scrollToRow(lines.length);
+  }, [lines, isScrollToBottom]);
+  
   const cache = new CellMeasurerCache({
     fixedWidth: true,
     minHeight: 15,
   });
+
+  const handleScrollToBottom = () => {
+    setScrollToBottom(!isScrollToBottom);
+  };
 
   const renderRow = ({
     index, key, parent, style,
@@ -36,22 +51,30 @@ function LogsPage() {
 
   return (
     <React.Fragment>
-      <div key="logs" className="bg-gray-700 text-white font-mono min-h-full">
+      <div key="logs" className="bg-gray-700 text-white font-mono flex flex-col min-h-full">
+        <div className="flex flex-row text-sm text-font-main bg-background-alt items-center">
+          <Checkbox id="scroll" isChecked={isScrollToBottom} onChange={handleScrollToBottom} />
+          <span className="pl-1">Scroll to bottom</span>
+        </div>
+        <div className={cx('flex flex-col grow overflow-hidden', lines.length > 0 && 'overflow-y-auto' )}>
         {lines.length === 0 && <span key="empty">Nothing here. Make sure you have server version that supports log streaming.</span>}
-        <AutoSizer>
+        <AutoSizer className="grow">
           {({ width, height }) => (
             <List
+              ref={listRef}
               deferredMeasurementCache={cache}
               rowHeight={cache.rowHeight}
               width={width}
               height={height}
               rowRenderer={renderRow}
               rowCount={lines.length}
-              overscanRowCount={0}
+              overscanRowCount={5}
               scrollToIndex={lines.length}
+              scrollToAlignment="end"
             />
           )}
         </AutoSizer>
+        </div>
       </div>
     </React.Fragment>
   );
