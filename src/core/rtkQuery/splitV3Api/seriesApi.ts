aa89c9c4e173228835354cd7fa34d3520cca48eb
Mikal S <revam@users.noreply.github.com>
2023-03-13 16:35:34 +0100

    fix: fix incomplete drop-down list in episode linking panel (#500)

--- src/core/rtkQuery/splitV3Api/seriesApi.ts
+++ src/core/rtkQuery/splitV3Api/seriesApi.ts
@@ -28,7 +28,7 @@ const seriesApi = splitV3Api.injectEndpoints({
 
     // Get the Shoko.Server.API.v3.Models.Shoko.Episodes for the Shoko.Server.API.v3.Models.Shoko.Series with seriesID.
     getSeriesEpisodes: build.query<ListResultType<EpisodeType[]>, { seriesId: number; } & PaginationType>({
-      query: ({ seriesId }) => ({ url: `Series/${seriesId}/Episode?includeMissing=true&includeDataFrom=AniDB&includeDataFrom=TvDB` }),
+      query: ({ seriesId, ...params }) => ({ url: `Series/${seriesId}/Episode?includeMissing=true&includeDataFrom=AniDB&includeDataFrom=TvDB`, params }),
       providesTags: ['SeriesEpisodes', 'UtilitiesRefresh'],
     }),
 
