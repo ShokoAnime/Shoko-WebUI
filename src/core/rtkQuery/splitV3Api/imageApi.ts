7496474c10544c4a2003dcd847b4d4ebd1212ad6
hidden4003 <hidden4003@users.noreply.github.com>
2023-04-08 17:01:04 +0300

    Login image series title (#519)
    * Show series title on the random login image

---
+++ src/core/rtkQuery/splitV3Api/imageApi.ts
@@ -0,0 +1,31 @@
+import { splitV3Api } from '../splitV3Api';
+import { ImageTypeEnum } from '../../types/api/common';
+
+type RandomMetadataResultType = {
+  Source: string;
+  Type: ImageTypeEnum;
+  ID: string;
+  RelativeFilepath: string;
+  Preferred: boolean;
+  Width: number | null;
+  Height: number | null;
+  Disabled: boolean;
+  Series: { 
+    ID: number;
+    Name: string;
+  }
+};
+
+const imageApi = splitV3Api.injectEndpoints({
+  endpoints: build => ({
+    getRandomMetadata: build.query<RandomMetadataResultType, { imageType: ImageTypeEnum }>({
+      query: ({ imageType }) => ({
+        url: `Image/Random/${imageType}/Metadata`,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetRandomMetadataQuery,
+} = imageApi;
