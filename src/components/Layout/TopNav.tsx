5d1fdace1cc0a131a337b6d07251dd529400d506
hidden4003 <hidden4003@users.noreply.github.com>
2023-04-10 10:25:35 +0300

    Draw the rest of the owl

--- src/components/Layout/TopNav.tsx
+++ src/components/Layout/TopNav.tsx
@@ -3,9 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Icon } from '@mdi/react';
 import {
   mdiChevronDown,
-  mdiCogOutline,
-  mdiFormatListBulletedSquare, mdiGithub, mdiHelpCircleOutline,
-  mdiLayersTripleOutline,
+  mdiCogOutline, mdiDownloadCircleOutline,
+  mdiFormatListBulletedSquare, mdiGithub, mdiHelpCircleOutline, mdiInformationOutline,
+  mdiLayersTripleOutline, mdiLoading,
   mdiServer,
   mdiTabletDashboard,
   mdiTextBoxOutline,
@@ -21,6 +21,14 @@ import { setLayoutEditMode } from '../../core/slices/mainpage';
 import { setStatus as setUtilitiesStatus } from '../../core/slices/modals/utilities';
 import { setStatus as setActionsStatus } from '../../core/slices/modals/actions';
 import { siDiscord } from 'simple-icons/icons';
+import semver from 'semver';
+import Version from '../../../public/version.json';
+import { useGetWebuiUpdateCheckQuery, useGetWebuiUpdateMutation } from '../../core/rtkQuery/splitV3Api/webuiApi';
+import { useGetSettingsQuery } from '../../core/rtkQuery/splitV3Api/settingsApi';
+import { initialSettings } from '../../pages/settings/SettingsPage';
+import Button from '../Input/Button';
+import toast from '../Toast';
+import { push } from '@lagunovsky/redux-react-router';
 
 function TopNav() {
   const dispatch = useDispatch();
@@ -28,10 +36,17 @@ function TopNav() {
   const queueItems = useSelector((state: RootState) => state.mainpage.queueStatus);
   const username = useSelector((state: RootState) => state.apiSession.username);
   const pathname = useSelector((state: RootState) => state.router.location.pathname);
+  const banStatus = useSelector((state: RootState) => state.mainpage.banStatus);
   const layoutEditMode = useSelector((state: RootState) => state.mainpage.layoutEditMode);
 
   const utilitiesModalOpen = useSelector((state: RootState) => state.modals.utilities.status);
   const actionsModalOpen = useSelector((state: RootState) => state.modals.actions.status);
+
+  const settingsQuery = useGetSettingsQuery();
+  const webuiSettings = settingsQuery?.data?.WebUI_Settings ?? initialSettings.WebUI_Settings;
+
+  const checkWebuiUpdate = useGetWebuiUpdateCheckQuery(webuiSettings.updateChannel, { skip: Version.debug || !settingsQuery.isSuccess });
+  const [webuiUpdateTrigger, webuiUpdateResult] = useGetWebuiUpdateMutation();
 
   const closeModalsAndSidebar = () => {
     dispatch(setUtilitiesStatus(false));
@@ -50,13 +65,40 @@ function TopNav() {
     }
   };
 
+  const handleWebUiUpdate = () => {
+    const renderToast = () => (
+      <div className="flex flex-col">
+        WebUI Update Successful!
+        <div className="flex items-center justify-end mt-3">
+          <Button onClick={() => {
+            toast.dismiss('webui-update');
+            dispatch(push('/webui/dashboard'));
+            setTimeout(() => window.location.reload(), 100);
+          }} className="bg-highlight-1 py-1.5 w-full">
+            Click here to reload
+          </Button>
+        </div>
+      </div>
+    );
+
+    webuiUpdateTrigger(webuiSettings.updateChannel).unwrap().then(() => {
+      toast.success('', renderToast(), {
+        autoClose: false,
+        draggable: false,
+        closeOnClick: false,
+        toastId: 'webui-update',
+        className: 'w-72 ml-auto',
+      });
+    }, error => console.error(error));
+  };
+
   const renderNonLinkMenuItem = (key: string, text: string, icon: string, onClick: () => void, modalOpen = false) => {
     const uri = `/webui/${key}`;
     const isHighlighted = pathname.startsWith(uri) || modalOpen;
     return (
-      <div key={key} className={cx(['cursor-pointer flex items-center w-full px-7', isHighlighted && 'text-highlight-1'])} onClick={onClick}>
+      <div key={key} className={cx(['cursor-pointer flex items-center ml-8 first:ml-0', isHighlighted && 'text-highlight-1'])} onClick={onClick}>
         <div className="w-6 flex items-center mr-3 my-3"><Icon path={icon} size={0.83} horizontal vertical rotate={180}/></div>
-        <span className="text-lg">{text}</span>
+        <span>{text}</span>
       </div>
     );
   };
@@ -65,24 +107,24 @@ function TopNav() {
     const uri = `/webui/${key}`;
     const isHighlighted = pathname.startsWith(uri);
     return (
-      <div className="flex items-center w-full px-7 justify-between">
+      <div className="flex items-center justify-between ml-8 first:ml-0">
         <Link key={key} className={cx(['cursor-pointer flex items-center w-full', isHighlighted && 'text-highlight-1'])} to={uri} onClick={() => closeModalsAndSidebar()}>
           <div className="w-6 flex items-center mr-3 my-3"><Icon path={icon} size={0.83} horizontal vertical rotate={180}/></div>
-          <span className="text-lg">{text}</span>
+          <span>{text}</span>
         </Link>
       </div>
     );
   };
 
   const renderMenuLink = (url: string, icon: string) => (
-    <div key={icon} className="cursor-pointer w-6 flex items-center" onClick={() => window.open(url, '_blank')}><Icon path={icon} size={0.83} horizontal vertical rotate={180} /></div>
+    <div key={icon} className="cursor-pointer flex items-center" onClick={() => window.open(url, '_blank')}><Icon path={icon} size={0.83} horizontal vertical rotate={180} /></div>
   );
 
   return (
     <div className="flex flex-col bg-background-alt drop-shadow-[0_2px_2px_rgba(0,0,0,0.25)] z-[100] text-sm font-semibold">
       <div className="flex justify-between px-8 py-4 items-center max-w-[120rem] w-full mx-auto">
-        <div className="flex space-x-2">
-          <ShokoIcon className="w-6" />
+        <div className="flex items-center space-x-2">
+          <ShokoIcon className="w-8" />
           <span className="text-xl font-semibold">Shoko</span>
         </div>
         <div className="flex space-x-8">
@@ -103,19 +145,47 @@ function TopNav() {
         </div>
       </div>
       <div className="bg-background-nav">
-        <div className="flex justify-between p-4 max-w-[120rem] w-full mx-auto">
+        <div className="flex justify-between px-8 py-4 max-w-[120rem] w-full mx-auto">
           <div className="flex">
             {renderMenuItem('dashboard', 'Dashboard', mdiTabletDashboard)}
-            {renderMenuItem('collection', 'Collection', mdiLayersTripleOutline)}
-            <div className={cx('transition-opacity flex', layoutEditMode && 'opacity-50 pointer-events-none')}>
+            <div className={cx('transition-opacity flex ml-8', layoutEditMode && 'opacity-50 pointer-events-none')}>
+              {renderMenuItem('collection', 'Collection', mdiLayersTripleOutline)}
               {renderNonLinkMenuItem('utilities', 'Utilities', mdiTools, () => toggleModal('utilities'), utilitiesModalOpen)}
               {renderNonLinkMenuItem('actions', 'Actions', mdiFormatListBulletedSquare, () => toggleModal('actions'), actionsModalOpen)}
               {renderMenuItem('log', 'Log', mdiTextBoxOutline)}
+              {renderNonLinkMenuItem('dashboard-settings', 'Dashboard Settings', mdiTabletDashboard, () => dispatch(setLayoutEditMode(true)), layoutEditMode)}
               {renderMenuItem('settings', 'Settings', mdiCogOutline)}
             </div>
           </div>
+          <div className="flex grow space-x-8 mr-8 justify-end">
+            {((checkWebuiUpdate.isSuccess && semver.gt(checkWebuiUpdate.data.Version, Version.package)) || checkWebuiUpdate.isFetching || true) && !webuiUpdateResult.isSuccess && (
+              <div className="flex items-center font-semibold cursor-pointer" onClick={() => handleWebUiUpdate()}>
+                <Icon
+                  path={checkWebuiUpdate.isFetching || webuiUpdateResult.isLoading ? mdiLoading : mdiDownloadCircleOutline}
+                  size={1}
+                  className={checkWebuiUpdate.isFetching || webuiUpdateResult.isLoading ? 'text-highlight-1' : 'text-highlight-2'}
+                  spin={checkWebuiUpdate.isFetching || webuiUpdateResult.isLoading}
+                />
+                <div className="flex ml-3">
+                  Web UI {webuiUpdateResult.isLoading ? 'Updating...' : (checkWebuiUpdate.isFetching ? 'Checking for update' : 'Update Available')}
+                </div>
+              </div>
+            )}
+            {/*TODO: This maybe works, maybe doesn't. Cannot test properly.*/}
+            {(banStatus?.udp?.updateType === 1 && banStatus?.udp?.value) && (
+              <div className="flex items-center font-semibold cursor-pointer">
+                <Icon path={mdiInformationOutline} size={1} className="text-highlight-4 mr-3"/>
+                AniDB UDP Ban Detected!
+              </div>
+            )}
+            {(banStatus?.http?.updateType === 2 && banStatus?.http?.value) && (
+              <div className="flex items-center font-semibold cursor-pointer">
+                <Icon path={mdiInformationOutline} size={1} className="text-highlight-4 mr-3"/>
+                AniDB HTTP Ban Detected!
+              </div>
+            )}
+          </div>
           <div className="flex space-x-5">
-            {renderNonLinkMenuItem('dashboard-settings', 'Dashboard Settings', mdiTabletDashboard, () => dispatch(setLayoutEditMode(true)), layoutEditMode)}
             {renderMenuLink('https://discord.gg/vpeHDsg', siDiscord.path)}
             {renderMenuLink('https://docs.shokoanime.com', mdiHelpCircleOutline)}
             {renderMenuLink('https://github.com/ShokoAnime', mdiGithub)}
