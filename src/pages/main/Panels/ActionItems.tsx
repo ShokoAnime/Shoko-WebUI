1663092d75a8ecfd17dca53b5329083cd020bc15
hidden4003 <hidden4003@users.noreply.github.com>
2023-04-11 21:33:04 +0300

    Need to toggle it

--- src/pages/main/Panels/ActionItems.tsx
+++ src/pages/main/Panels/ActionItems.tsx
@@ -7,11 +7,10 @@ import Events from '../../../core/events';
 import quickActions from '../../../core/quick-actions';
 import FixedPanel from '../../../components/Panels/FixedPanel';
 import Button from '../../../components/Buttons/Button';
-import { removeAction } from '../../../core/slices/webuiSettings';
 
 class ActionItems extends React.Component<Props> {
   renderRow = (key: string) => {
-    const { runAction, deleteAction } = this.props;
+    const { runAction, togglePinnedAction } = this.props;
     const action = quickActions[key];
 
     if (!action) {
@@ -19,7 +18,7 @@ class ActionItems extends React.Component<Props> {
         <div className="flex justify-between items-center mt-3 first:mt-0" key={key}>
           <span className="flex line-through">{key}</span>
           <div className="flex">
-            <Button onClick={() => deleteAction(key)} className="bg-color-danger font-exo text-xs font-bold px-6 py-1">
+            <Button onClick={() => togglePinnedAction(key)} className="bg-color-danger font-exo text-xs font-bold px-6 py-1">
               Remove
             </Button>
           </div>
@@ -64,7 +63,9 @@ const mapDispatch = {
   runAction: (key: string, data: any) => (
     { type: Events.QUICK_ACTION_RUN, payload: { key, data } }
   ),
-  deleteAction: (key: string) => removeAction(key),
+  togglePinnedAction: (payload: string) => (
+    { type: Events.SETTINGS_TOGGLE_PINNED_ACTION, payload }
+  ),
 };
 
 const connector = connect(mapState, mapDispatch);
