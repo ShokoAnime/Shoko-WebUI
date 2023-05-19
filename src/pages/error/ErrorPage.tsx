58bea7409d24c1e845b063501059c5723f07a412
Harshith Mohan <gharshitmohan@ymail.com>
2023-05-19 20:24:55 +0530

    Update ErrorPage

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,23 +1,15 @@
-import * as Sentry from '@sentry/react';
 import React from 'react';
-import history from '../../core/history';
-import Link from '../../components/Link/Link';
+import * as Sentry from '@sentry/react';
+import { Link, useNavigate } from 'react-router-dom';
 
 type Props = {
-  error?: Error;
   children?: any;
 };
 
-type State = {
-  hasError: boolean;
-  error: {};
-  info: {
-    componentStack?: {};
-  };
-};
+const Fallback = ({ error, componentStack }) => {
+  const navigate = useNavigate();
 
-class ErrorBoundary extends React.Component<Props, State> {
-  fallback = ({ error, componentStack }) => (
+  return (
     <div className="error-page flex h-screen">
       <div className="m-auto">
         <h1 className="code">ERROR</h1>
@@ -26,19 +18,18 @@ class ErrorBoundary extends React.Component<Props, State> {
         <p className="title">{error.toString()}</p>
         <p className="text">Trace:<pre>{componentStack}</pre></p>
         <p className="text">
-          <a href="/" onClick={(event) => { event.preventDefault(); history.back(); }}>Go back</a>, or head over to the&nbsp;
+          <span className="text-highlight-1 cursor-pointer" onClick={() => navigate(-1)}>Go back</span>, or head over to the&nbsp;
           <Link to="/">home page</Link> to choose a new direction.
         </p>
       </div>
     </div>
   );
+};
 
-  render() {
-    const { children } = this.props;
-    return (
-      <Sentry.ErrorBoundary fallback={this.fallback} showDialog>{children}</Sentry.ErrorBoundary>
-    );
-  }
-}
+const ErrorBoundary = ({ children }: Props) => (
+  <Sentry.ErrorBoundary fallback={Fallback} showDialog>
+    {children}
+  </Sentry.ErrorBoundary>
+);
 
 export default ErrorBoundary;
