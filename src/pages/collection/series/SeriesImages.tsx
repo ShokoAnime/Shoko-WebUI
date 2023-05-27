214dba8263d710681b06b27401654f16b997c47f
hidden4003 <hidden4003@users.noreply.github.com>
2023-05-27 20:14:51 +0300

    Update series images page wip

--- src/pages/collection/series/SeriesImages.tsx
+++ src/pages/collection/series/SeriesImages.tsx
@@ -6,9 +6,10 @@ import BackgroundImagePlaceholderDiv from '@/components/BackgroundImagePlacehold
 import cx from 'classnames';
 import { useGetSeriesImagesQuery } from '@/core/rtkQuery/splitV3Api/seriesApi';
 import { ImageType } from '@/core/types/api/common';
+import Checkbox from '@/components/Input/Checkbox';
 
 const Heading = React.memo(({ type, setType }:{ type: string; setType: Function; }) => (
-  <React.Fragment>
+  <div className="font-semibold text-xl">
     Images
     <span className="px-2">&gt;</span>
     <span onClick={() => { setType('Posters'); }} className={cx(type === 'Posters' && 'text-highlight-1')}>Poster</span>
@@ -16,7 +17,7 @@ const Heading = React.memo(({ type, setType }:{ type: string; setType: Function;
     <span onClick={() => { setType('Fanarts'); }} className={cx(type === 'Fanarts' && 'text-highlight-1')}>Fanart</span>
     <span className="px-2">|</span>
     <span onClick={() => { setType('Banners'); }} className={cx(type === 'Banners' && 'text-highlight-1')}>Banners</span>
-  </React.Fragment>
+  </div>
 ));
 
 const getThumbnailUrl = (thumbnail: ImageType) => {
@@ -48,30 +49,42 @@ const SeriesImages = () => {
   const filepath = splitPath.join('/');
   
   const sizeMap = {
-    'Posters': 'h-[21.375rem] w-[15.2678rem]',
-    'Fanarts': 'h-[19.2375rem] w-[34.2rem]',
-    'Banners': 'h-[19.2375rem] w-[104.157rem]',
+    'Posters': 'h-[20.0625rem] w-[13.75rem]',
+    'Fanarts': 'h-[16rem] w-[28.5rem]',
+    'Banners': 'h-[8rem] w-[43.25rem]',
   };
 
   return (
-    <div className="flex space-x-9">
-      <ShokoPanel className="grow" title={<Heading type={type} setType={setType} />}>
-        <div className="flex flex-wrap">
+    <div className="flex space-x-8">
+      <div className="grow-0 shrink-0 w-[22.375rem] flex flex-col align-top space-y-8">
+        <ShokoPanel title="Image Options" transparent className="grow-0">
+          <div className="flex flex-col space-y-1">
+            <Checkbox id="random-poster" isChecked={false} onChange={() => {}} label="Random Poster on Load" justify />
+            <Checkbox id="random-fanart" isChecked={false} onChange={() => {}} label="Random Fanart on Load" justify />
+          </div>
+        </ShokoPanel>
+        <ShokoPanel title="Selected Image Info" transparent className="min-h-[25rem]">
+          <div className="flex flex-col space-y-4">
+            <InfoLine title="Filename" value={filename}/>
+            <InfoLine title="Location" value={filepath}/>
+            <InfoLine title="Source" value={selectedImage?.Source}/>
+            <InfoLine title="Size" value="--"/>
+          </div>
+        </ShokoPanel>
+      </div>
+      <div className="flex flex-col grow space-y-8">
+        <div className="rounded bg-background-alt/25 px-8 py-4 flex justify-between items-center border-background-border border">
+          <Heading type={type} setType={setType} />
+          <div className="font-semibold text-xl"><span className="text-highlight-2">{get(images, type, []).length}</span> {type} Listed</div>
+        </div>
+        <div className="flex flex-wrap gap-4 rounded bg-background-alt/25 p-8 border-background-border border">
           {map(get(images, type, []), (item: ImageType) => (
             <div onClick={() => { setSelectedImage(item); }} key={item?.ID}>
-              <BackgroundImagePlaceholderDiv imageSrc={getThumbnailUrl(item)} className={cx('rounded drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] border my-2 mr-4', item === selectedImage ? 'border-highlight-2 border-2 opacity-30' : 'border-black', sizeMap[type] )} />
+              <BackgroundImagePlaceholderDiv imageSrc={getThumbnailUrl(item)} className={cx('rounded drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] border', item === selectedImage ? 'border-highlight-2 border-2 opacity-50' : 'border-black', sizeMap[type] )} />
             </div>
           ))}
         </div>
-      </ShokoPanel>
-      <ShokoPanel className="shrink-0 min-w-[23rem]" title="Selected Image Info">
-        <div className="flex flex-col space-y-4">
-          <InfoLine title="Filename" value={filename}/>
-          <InfoLine title="Location" value={filepath}/>
-          <InfoLine title="Source" value={selectedImage?.Source}/>
-          <InfoLine title="Size" value="--"/>
-        </div>
-      </ShokoPanel>
+      </div>
     </div>
   );
 };
