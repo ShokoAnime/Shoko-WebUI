48a060fa1c68576687d0297d06cd1f3609756b3e
Daniel <Daniel@DPC>
2023-05-31 19:12:52 +0300

    Pulse targets on collection jump

--- src/pages/collection/GroupList.tsx
+++ src/pages/collection/GroupList.tsx
@@ -1,57 +1,97 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { AutoSizer, Grid } from 'react-virtualized';
-import { useDispatch, useSelector } from 'react-redux';
-import { debounce, find, memoize, reduce } from 'lodash';
+import React, { useEffect, useRef, useState } from "react";
+import { AutoSizer, Grid } from "react-virtualized";
+import { useDispatch, useSelector } from "react-redux";
+import { debounce, find, memoize, reduce } from "lodash";
 
-import ListViewGroupItem from './items/ListViewGroupItem';
-import GroupPlaceholder from './items/GroupPlaceholder';
-import GridViewGroupItem from './items/GridViewGroupItem';
-import GridOptions from './items/GridOptions';
+import ListViewGroupItem from "./items/ListViewGroupItem";
+import GroupPlaceholder from "./items/GroupPlaceholder";
+import GridViewGroupItem from "./items/GridViewGroupItem";
+import GridOptions from "./items/GridOptions";
 
-import { setStatus } from '../../core/slices/modals/filters';
-import ShokoPanel from '../../components/Panels/ShokoPanel';
+import { setStatus } from "../../core/slices/modals/filters";
+import ShokoPanel from "../../components/Panels/ShokoPanel";
 import {
   useGetGroupLettersQuery,
   useLazyGetGroupsQuery,
-} from '../../core/rtkQuery/splitV3Api/collectionApi';
+} from "../../core/rtkQuery/splitV3Api/collectionApi";
 
-import { resetGroups, setGroups } from '../../core/slices/collection';
-import { useLazyGetGroupViewQuery } from '../../core/rtkQuery/splitV3Api/webuiApi';
+import { resetGroups, setGroups } from "../../core/slices/collection";
+import { useLazyGetGroupViewQuery } from "../../core/rtkQuery/splitV3Api/webuiApi";
 
-import type { RootState } from '../../core/store';
-import Jumpbar from './Jumpbar';
+import type { RootState } from "../../core/store";
+import Jumpbar from "./Jumpbar";
 
 function GroupList() {
   const itemWidth = 240; //224 + 16
   const itemHeight = 374; //358 + 16
   const itemHeightList = 240; //176 + 16 + 16
   const pageSize = 50;
-  const fetchedPages = useSelector((state: RootState) => state.collection.fetchedPages);
-  const total: number = useSelector((state: RootState) => state.collection.total);
+  const fetchedPages = useSelector(
+    (state: RootState) => state.collection.fetchedPages
+  );
+  const total: number = useSelector(
+    (state: RootState) => state.collection.total
+  );
   const dispatch = useDispatch();
-  const [mode, setMode] = useState('grid');
+  const [mode, setMode] = useState("grid");
   const [trigger] = useLazyGetGroupsQuery();
   const [fetchMainGroups, mainGroups] = useLazyGetGroupViewQuery();
-  const letters = useGetGroupLettersQuery({ includeEmpty: false, topLevelOnly: true });
+  const letters = useGetGroupLettersQuery({
+    includeEmpty: false,
+    topLevelOnly: true,
+  });
   const gridRef = useRef<Grid>(null);
   const [columns, setColumns] = useState(0);
+  const [highlightLetter, setHighlightLetter] = useState("");
 
-  const toggleMode = () => { setMode(mode === 'list' ? 'grid' : 'list'); };
+  const toggleMode = () => {
+    setMode(mode === "list" ? "grid" : "list");
+  };
 
-  const fetchPage = debounce(memoize((page) => {
-    trigger({ page, pageSize }).then((result) => {
-      if (!result.data) { return; }
-      dispatch(setGroups({ total: result.data.Total, items: result.data.List, page }));
-      
-      //TODO: figure out how to only fetch it in list mode
-      const ids = reduce(result.data.List, (out, value) => { 
-        out.push(value.IDs.ID); 
-        return out;
-      }, [] as Array<number>);
-      fetchMainGroups({ GroupIDs: ids, TagFilter: 128, TagLimit: 20, OrderByName: true }).then(() => {}, (reason) => { console.error(reason); });
-    }, (reason) => { console.error(reason); });
-    return true;
-  }), 200);
+  const fetchPage = debounce(
+    memoize((page) => {
+      trigger({ page, pageSize }).then(
+        (result) => {
+          if (!result.data) {
+            return;
+          }
+          dispatch(
+            setGroups({
+              total: result.data.Total,
+              items: result.data.List,
+              page,
+            })
+          );
+
+          //TODO: figure out how to only fetch it in list mode
+          const ids = reduce(
+            result.data.List,
+            (out, value) => {
+              out.push(value.IDs.ID);
+              return out;
+            },
+            [] as Array<number>
+          );
+          fetchMainGroups({
+            GroupIDs: ids,
+            TagFilter: 128,
+            TagLimit: 20,
+            OrderByName: true,
+          }).then(
+            () => {},
+            (reason) => {
+              console.error(reason);
+            }
+          );
+        },
+        (reason) => {
+          console.error(reason);
+        }
+      );
+      return true;
+    }),
+    200
+  );
 
   useEffect(() => {
     dispatch(resetGroups());
@@ -65,51 +105,85 @@ function GroupList() {
     dispatch(setStatus(true));
   };
 
-  const renderTitle = count => (
+  const renderTitle = (count) => (
     <React.Fragment>
       Entire Collection
-      <span className="px-2">|</span>
-      <span className="text-highlight-2">{count} Items</span>
+      <span className='px-2'>|</span>
+      <span className='text-highlight-2'>{count} Items</span>
     </React.Fragment>
   );
 
-  const Cell = cols => ({ columnIndex, key, rowIndex, style }) => {
-    const index = rowIndex * cols + columnIndex;
-    const neededPage = Math.ceil((index + 1) / pageSize);
-    const groupList = fetchedPages[neededPage];
-    if (cols !== columns) {
-      setColumns(cols);
-    }
-    if (groupList == undefined) {
-      fetchPage(neededPage);
+  const Cell =
+    (cols) =>
+    ({ columnIndex, key, rowIndex, style }) => {
+      const index = rowIndex * cols + columnIndex;
+      const neededPage = Math.ceil((index + 1) / pageSize);
+      const groupList = fetchedPages[neededPage];
+      if (cols !== columns) {
+        setColumns(cols);
+      }
+      if (groupList == undefined) {
+        fetchPage(neededPage);
+        return <GroupPlaceholder id={key} style={style} />;
+      }
+      const item = groupList[index % pageSize];
+      if (!item) {
+        return null;
+      }
+      const shouldHighlight =
+        highlightLetter.toLowerCase() === item.Name[0].toLowerCase();
       return (
-        <GroupPlaceholder id={key} style={style}/>
+        <div
+          key={key}
+          style={style}
+          className={`${shouldHighlight ? "animate-pulse" : ""}`}
+        >
+          {mode === "grid"
+            ? GridViewGroupItem(item)
+            : ListViewGroupItem(
+                item,
+                find(mainGroups?.data, ["ID", item?.IDs.ID])
+              )}
+          {mode === "list" && (
+            <div className='bg-background-border my-4 h-0.5 w-full' />
+          )}
+        </div>
       );
-    }
-    const item = groupList[index % pageSize];
-    if (!item) { return null; }
-    return (
-    <div key={key} style={style}>
-      {mode === 'grid' ? GridViewGroupItem(item) : ListViewGroupItem(item, find(mainGroups?.data, ['ID', item?.IDs.ID]))}
-      {mode === 'list' && <div className="bg-background-border my-4 h-0.5 w-full" />}
-    </div>
-    );
-  };
+    };
 
   return (
-    <div className="p-9 pr-0 h-full min-w-full flex">
-      <ShokoPanel className="grow" title={renderTitle(total)} options={<GridOptions showFilters={showFilters} toggleMode={toggleMode} />}>
+    <div className='p-9 pr-0 h-full min-w-full flex'>
+      <ShokoPanel
+        className='grow'
+        title={renderTitle(total)}
+        options={
+          <GridOptions showFilters={showFilters} toggleMode={toggleMode} />
+        }
+      >
         <AutoSizer>
           {({ width, height }) => {
-            const gridColumns = mode === 'grid' ? Math.floor(width / itemWidth) : 1;
-            const rows = mode === 'grid' ? Math.ceil(total / gridColumns) : total;
+            const gridColumns =
+              mode === "grid" ? Math.floor(width / itemWidth) : 1;
+            const rows =
+              mode === "grid" ? Math.ceil(total / gridColumns) : total;
             return (
-              <Grid ref={gridRef} className="grow" overscanRowCount={1} columnCount={gridColumns} rowCount={rows} columnWidth={mode === 'grid' ? itemWidth : width - 32} height={height} rowHeight={mode === 'grid' ? itemHeight : itemHeightList} width={width} cellRenderer={Cell(gridColumns)} />
+              <Grid
+                ref={gridRef}
+                className='grow'
+                overscanRowCount={1}
+                columnCount={gridColumns}
+                rowCount={rows}
+                columnWidth={mode === "grid" ? itemWidth : width - 32}
+                height={height}
+                rowHeight={mode === "grid" ? itemHeight : itemHeightList}
+                width={width}
+                cellRenderer={Cell(gridColumns)}
+              />
             );
           }}
         </AutoSizer>
       </ShokoPanel>
-      <Jumpbar items={letters?.data} columns={columns} gridRef={gridRef} />
+      <Jumpbar items={letters?.data} columns={columns} gridRef={gridRef} onJump={key => setHighlightLetter(key)}/>
     </div>
   );
 }
--- src/pages/collection/Jumpbar.tsx
+++ src/pages/collection/Jumpbar.tsx
@@ -1,19 +1,40 @@
-import { map } from 'lodash';
-import React from 'react';
+import { map } from "lodash";
+import React, { RefObject } from "react";
+import { Grid } from "react-virtualized";
 
-const Jumpbar = ({ items, columns, gridRef }) => {
+export interface JumpBarProps {
+  items: any;
+  columns: any;
+  gridRef: RefObject<Grid>;
+  onJump?: ((key: string) => void) | undefined;
+}
+
+const Jumpbar = ({
+  items,
+  columns,
+  gridRef,
+  onJump = undefined,
+}: JumpBarProps) => {
   if (!items || columns === 0) {
     return null;
   }
   let index = 0;
   return (
-    <div
-      className="shrink-0 px-6 pb-9 shrink-0 flex flex-col drop-shadow-[-4px_0_4px_rgba(0,0,0,0.25)]">
+    <div className='shrink-0 px-6 pb-9 shrink-0 flex flex-col drop-shadow-[-4px_0_4px_rgba(0,0,0,0.25)]'>
       {map(items, (count, key) => {
         const rowIndex = Math.ceil(index / columns);
-        const item = <div className="cursor-pointer my-1" onClick={() => {
-          gridRef?.current?.scrollToCell({ columnIndex: 0, rowIndex });
-        }} key={key}>{key}</div>;
+        const item = (
+          <div
+            className='cursor-pointer my-1'
+            onClick={() => {
+              gridRef?.current?.scrollToCell({ columnIndex: 0, rowIndex });
+              if (onJump) onJump(key);
+            }}
+            key={key}
+          >
+            {key}
+          </div>
+        );
         index += count;
         return item;
       })}
