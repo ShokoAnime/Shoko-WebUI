e80ce5a5f39a47e06bdab425b5f9fa603fcd5f90
Harshith Mohan <gharshitmohan@ymail.com>
2023-06-03 20:23:38 +0530

    Update ShokoNews Panel

--- src/pages/dashboard/panels/ShokoNews.tsx
+++ src/pages/dashboard/panels/ShokoNews.tsx
@@ -9,26 +9,26 @@ import { DashboardNewsType } from '@/core/types/api/dashboard';
 
 import { useGetShokoNewsFeedQuery } from '@/core/rtkQuery/externalApi';
 
+const NewsRow = ({ item }: { item: DashboardNewsType }) => (
+  <div className="flex flex-col gap-y-1" key={item.title}>
+    <p className="font-semibold opacity-75">{item.date_published}</p>
+    <p className="font-semibold">{item.title}</p>
+    <a href={item.link} rel="noopener noreferrer" target="_blank" className="flex gap-x-2 text-highlight-1 font-semibold items-center mt-1">
+      Read More
+      <Icon path={mdiOpenInNew} size={1} />
+    </a>
+  </div>
+);
+
 function ShokoNews() {
   const layoutEditMode = useSelector((state: RootState) => state.mainpage.layoutEditMode);
 
   const items = useGetShokoNewsFeedQuery();
 
-  const renderRow = (item: DashboardNewsType) => (
-    <div className="flex flex-col space-y-1 mt-1 first:mt-0" key={item.title}>
-        <p className="text-base font-semibold opacity-75">{item.date_published}</p>
-        <p className="text-base font-semibold">{item.title}</p>
-      <div className="flex gap-x-2 text-highlight-1 font-semibold">
-        <a href={item.link} rel="noopener,noreferrer" target="_blank">Read More</a>
-        <Icon path={mdiOpenInNew} size={1} />
-      </div>
-    </div>
-  );
-
   return (
     <ShokoPanel title="Shoko News" isFetching={items.isLoading} editMode={layoutEditMode}>
-      <div className="flex flex-col space-y-3">
-        {items.data?.slice(0, 4).map(item => renderRow(item))}
+      <div className="flex flex-col gap-y-3">
+        {items.data?.slice(0, 4).map(item => <NewsRow item={item} />)}
       </div>
     </ShokoPanel>
   );
