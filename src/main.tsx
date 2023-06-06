47250456625df187a4a2798ec259f7341576ac72
funniray <funniray10@gmail.com>
2023-06-06 18:28:10 +0100

    Only enable sentry when in production, remove the deprecated `@sentry/tracing` package

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { Integrations } from '@sentry/tracing';
 import * as Sentry from '@sentry/react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
@@ -6,21 +5,23 @@ import App from './core/app';
 import { uiVersion, isDebug } from './core/util';
 import './css/main.css';
 
-Sentry.init({
-  dsn: 'https://f607489ccc764d73aeaed81ab2c97c04@o330862.ingest.sentry.io/1851857',
-  environment: isDebug() ? 'staging' : 'production',
-  release: isDebug() ? 'dev' : uiVersion(),
-  integrations: [
-    new Integrations.BrowserTracing(),
-    new Sentry.Replay({
-      maskAllText: true,
-      blockAllMedia: false,
-    }),
-  ],
-  tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+if (!isDebug()) {
+  Sentry.init({
+    dsn: 'https://f607489ccc764d73aeaed81ab2c97c04@o330862.ingest.sentry.io/1851857',
+    environment: 'production',
+    release: uiVersion(),
+    integrations: [
+      new Sentry.BrowserTracing(),
+      new Sentry.Replay({
+        maskAllText: true,
+        blockAllMedia: false,
+      }),
+    ],
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
 
 const container = document && document.getElementById('app-root');
 
