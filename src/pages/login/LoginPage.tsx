02691c7b6208983aff3d4fa94c6dc13a92571b78
Harshith Mohan <gharshitmohan@ymail.com>
2023-06-06 14:50:03 +0530

    Update login page to new design

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -7,7 +7,7 @@ import { get } from 'lodash';
 import { Slide, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import { Icon } from '@mdi/react';
-import { mdiCloseCircle, mdiLoading } from '@mdi/js';
+import { mdiCloseCircleOutline, mdiGithub, mdiHelpCircleOutline, mdiLoading } from '@mdi/js';
 import { siDiscord } from 'simple-icons';
 
 import { RootState } from '@/core/store';
@@ -86,79 +86,85 @@ function LoginPage() {
         closeButton={false}
         icon={false}
       />
-      <div className="flex h-screen w-screen">
-        <div className={cx('flex grow login-image', loginImage === 'default' && 'login-image-default')} style={loginImage !== '' && loginImage !== 'default'  ? { backgroundImage: `url('${loginImage}')` } : {}}/>
-        <div className="absolute top-1.5 right-[32rem] text-font-main bg-background-nav px-2 py-1 font-semibold text-2xl">{loginSeriesTitle}</div>
-        <div className="flex flex-col flex-none p-12 items-center justify-between w-[31.25rem] bg-background-alt border-l border-background-border">
-          <ShokoIcon className="w-32" />
-          <div className="flex items-center font-semibold mt-4">
-            Version: {version.isFetching || !version.data ?
-              <Icon path={mdiLoading} spin size={1} className="ml-2 text-highlight-1" /> :
-            version.data.Server.ReleaseChannel !== 'Stable' ?
-              `${version.data.Server.Version}-${version.data.Server.ReleaseChannel} (${version.data.Server.Commit?.slice(0, 7)})` :
-              version.data.Server.Version
-            }
+      <div className={cx('flex h-screen w-screen login-image items-center justify-center relative', loginImage === 'default' && 'login-image-default')} style={loginImage !== '' && loginImage !== 'default'  ? { backgroundImage: `url('${loginImage}')` } : {}}>
+        <div className="absolute top-0 right-0 bg-background-alt/90 px-8 py-4 font-semibold border border-background-border">{imageMetadata.isError ? 'Spy X Family' : loginSeriesTitle}</div>
+
+        <div className="flex flex-col items-center p-8 w-[31.25rem] bg-background-alt/90 border border-background-border rounded-md gap-y-8">
+          <div className="flex flex-col gap-y-4 items-center">
+            <ShokoIcon className="w-24" />
+            <div className="font-semibold">
+              Version: {version.isFetching || !version.data ?
+                <Icon path={mdiLoading} spin size={1} className="ml-2 text-highlight-1" /> :
+              version.data.Server.ReleaseChannel !== 'Stable' ?
+                `${version.data.Server.Version}-${version.data.Server.ReleaseChannel} (${version.data.Server.Commit?.slice(0, 7)})` :
+                version.data.Server.Version
+              }
+            </div>
           </div>
-          <div className="flex flex-col grow w-full justify-center p-1">
+
+          <div className="flex flex-col w-full gap-y-4">
             {!status.data?.State && (
               <div className="flex justify-center items-center">
-                <Icon path={mdiLoading} spin className="text-highlight-1" size={5} />
+                <Icon path={mdiLoading} spin className="text-highlight-1" size={4} />
               </div>
             )}
             {status.data?.State === 1 && (
-              <div className="flex flex-col justify-center items-center">
+              <div className="flex flex-col justify-center items-center gap-y-2">
                 <Icon path={mdiLoading} spin className="text-highlight-1" size={4} />
-                <div className="mt-4 text-xl font-semibold">Server is starting. Please wait!</div>
-                <div className="mt-2 text-lg">
+                <div className="mt-2 text-xl font-semibold">Server is starting. Please wait!</div>
+                <div className="text-lg">
                   <span className="font-semibold">Status: </span>{status.data?.StartupMessage ?? 'Unknown'}
                 </div>
               </div>
             )}
             {status.data?.State === 2 && (
-              <React.Fragment>
-                <form className="-mt-32" onSubmit={handleSignIn}>
-                  <div className="flex flex-col">
-                    <Input autoFocus id="username" value={username} label="Username" type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
-                    <Input id="password" value={password} label="Password" type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} className="mt-4" />
-                  </div>
-                  <Checkbox id="rememberUser" label="Remember Me" isChecked={rememberUser} onChange={e => setRememberUser(e.target.checked)} className="font-semibold mt-4" labelRight />
-                  <Button className="bg-highlight-1 mt-4 py-2 w-full font-semibold" type="submit" loading={isFetchingLogin} disabled={version.isFetching || username === ''}>Login</Button>
-                </form>
-              </React.Fragment>
+              <form onSubmit={handleSignIn} className="flex flex-col gap-y-8">
+                <Input autoFocus id="username" value={username} label="Username" type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+                <Input id="password" value={password} label="Password" type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+                <Checkbox id="rememberUser" label="Remember Me" isChecked={rememberUser} onChange={e => setRememberUser(e.target.checked)} className="font-semibold" labelRight />
+                <Button className="bg-highlight-1 py-2 w-full font-semibold" type="submit" loading={isFetchingLogin} disabled={version.isFetching || username === ''}>Login</Button>
+              </form>
             )}
             {status.data?.State === 3 && (
-              <div className="flex flex-col justify-center items-center overflow-y-auto pb-2">
-                <Icon path={mdiCloseCircle} className="text-highlight-3" size={4} />
-                <div className="mt-4 text-xl font-semibold">Server startup failed!</div>
+              <div className="flex flex-col justify-center items-center pb-2 gap-y-2 max-h-[20rem]">
+                <Icon path={mdiCloseCircleOutline} className="text-highlight-3 shrink-0" size={4} />
+                <div className="mt-2 text-xl font-semibold">Server startup failed!</div>
                 Check the error message below
-                <div className="mt-2 text-lg break-all overflow-y-auto font-semibold">{status.data?.StartupMessage ?? 'Unknown'}</div>
+                <div className="text-lg break-all overflow-y-auto font-semibold">{status.data?.StartupMessage ?? 'Unknown'}</div>
               </div>
             )}
             {status.data?.State === 4 && (
-              <div className="flex flex-col -mt-32 gap-y-4">
+              <div className="flex flex-col gap-y-4">
                 <div className="flex flex-col gap-y-4">
                   <div className="font-semibold">First Time? We&apos;ve All Been There</div>
                   <div className="text-justify">
                     Before Shoko can get started indexing your anime collection, you&apos;ll
-                    need to go through our <span className="text-highlight-2">First Run Wizard </span>
+                    need to go through our <span className="text-highlight-2 font-semibold">First Run Wizard </span>
                     and set everything up. Don&apos;t worry, it&apos;s pretty easy and only
                     takes a couple of minutes.
                   </div>
                   <div>
-                    Click <span className="text-highlight-2">Continue</span> below to proceed.
+                    Click <span className="text-highlight-2 font-semibold">Continue</span> below to proceed.
                   </div>
                 </div>
                 <Button onClick={() => navigate('/webui/firstrun')} className="bg-highlight-1 py-2 font-semibold">Continue</Button>
               </div>
             )}
           </div>
-          <div className="flex flex-col w-full font-semibold">
-            <Button className="flex bg-highlight-1 items-center justify-center py-2" onClick={() => window.open('https://discord.gg/vpeHDsg', '_blank')}>
-              Get Help on <Icon path={siDiscord.path} size={0.75} className="mx-1" /> Discord
-            </Button>
-            <Button className="bg-highlight-1 py-2 mt-4" onClick={() => window.open('https://docs.shokoanime.com', '_blank')}>
-              Documentation
-            </Button>
+
+          <div className="flex font-semibold gap-x-8">
+            <a href="https://discord.gg/vpeHDsg" target="_blank" rel="noopener noreferrer" className="flex items-center gap-x-2">
+              <Icon path={siDiscord.path} size={1} />
+              Discord
+            </a>
+            <a href="https://docs.shokoanime.com" target="_blank" rel="noopener noreferrer" className="flex items-center gap-x-2">
+              <Icon path={mdiHelpCircleOutline} size={1} />
+              Docs
+            </a>
+            <a href="https://github.com/ShokoAnime" target="_blank" rel="noopener noreferrer" className="flex items-center gap-x-2">
+              <Icon path={mdiGithub} size={1} />
+              GitHub
+            </a>
           </div>
         </div>
       </div>
