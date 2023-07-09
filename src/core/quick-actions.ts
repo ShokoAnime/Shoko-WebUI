af34f23d6e542fb82db265a733cb98646f0d20b3
Terrails <github@sl.terrails.com>
2023-07-09 15:09:04 +0200

    Add action for RenameAllGroups endpoint (#584)

--- src/core/quick-actions.ts
+++ src/core/quick-actions.ts
@@ -122,6 +122,11 @@ const quickActions = {
     data: false,
     info: 'Deletes all existing groups in Shoko and recreates them',
   },
+  'rename-all-groups': {
+    name: 'Rename All Groups',
+    functionName: 'RenameAllGroups',
+    info: 'Renames any groups without a custom name set based on the current language preference.',
+  },
 } as {
   [key: string]: {
     name: string;
