593de059651d963f233e2014612ada0667c734c3
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2023-08-08 02:44:14 -0700

    Add Indicator For New News Post. (#616)
    * Add Indicator For New News Post.

--- src/pages/dashboard/panels/ShokoNews.tsx
+++ src/pages/dashboard/panels/ShokoNews.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Icon } from '@mdi/react';
 import { mdiOpenInNew } from '@mdi/js';
+import moment from 'moment';
 
 import { RootState } from '@/core/store';
 import ShokoPanel from '@/components/Panels/ShokoPanel';
@@ -9,9 +10,19 @@ import { DashboardNewsType } from '@/core/types/api/dashboard';
 
 import { useGetShokoNewsFeedQuery } from '@/core/rtkQuery/externalApi';
 
+const newNewsCheck = (date: string) => {
+  const itemDate = moment(date);
+  const currentDate = moment();
+  const differenceInDays = currentDate.diff(itemDate, 'days');
+  return differenceInDays <= 14;
+};
+
 const NewsRow = ({ item }: { item: DashboardNewsType }) => (
   <div className="flex flex-col" key={item.title}>
-    <p className="font-semibold">{item.date_published}</p>
+    <div className="flex gap-x-4 font-semibold">
+      <p>{item.date_published}</p>
+      {newNewsCheck(item.date_published) && (<p className="text-panel-important">New Post</p>)}
+    </div>
     <a href={item.link} rel="noopener noreferrer" target="_blank" className="flex text-panel-primary font-semibold items-center mt-1 space-x-2 hover:text-panel-primary-hover">
       <p className="font-semibold">{item.title}</p>
       <Icon path={mdiOpenInNew} size={1} />
