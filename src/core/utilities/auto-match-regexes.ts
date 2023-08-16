12e6c4deaf82c9162ae17f34624d30879effeea8
Mikal Stordal <mikalstordal@gmail.com>
2023-08-16 20:37:22 +0200

    misc: more refinement to the general regex

--- src/core/utilities/auto-match-regexes.ts
+++ src/core/utilities/auto-match-regexes.ts
@@ -4,7 +4,14 @@ const PathMatchRuleSet: PathMatchRule[] = [
   {
     name: 'general',
     regex:
-      /^(?:[{[](?<releaseGroup>[a-zA-Z0-9_-]+)[}\]]\s+)?(?<showName>.+?(?:\((?:19|20)\d{2}\)\s+)?)\s+(?:-\s+)?(?:(?:(?<isThemeSong>(?:NC)?(?:ED|OP)\s*))?|(?<isSpecial>SP?)?|E?)(?<episode>\d+(?:-\d+|\.\d+)?)(?:[vV](?<version>\d{1,2}))?(?:\s+(?:\((?:(?:\b(?:\d{3,4}[pPiI]|\d{3,4}x\d{3,4}|Dual Audio|BD|RIP|FLAC(?:\s+[257]\.[0124])?)\b\s*)+)\)|\[(?:(?:\d{3,4}[pPiI]|\d{3,4}x\d{3,4}|Dual Audio|BD|RIP|FLAC(?:\s+[257]\.[0124])?)\s*)+\]))*(?:\s+\[(?<crc32>[0-9a-fA-F]{8})\])?\.(?<extension>[a-zA-Z0-9_\-+]+)$/,
+      /^(?:[{[(](?<releaseGroup>[a-zA-Z0-9_-]+)[)}\]][\s_]*)?(?<showName>.+?(?:[\s_]*\((?<year>(?:19|20)\d{2})\)[\s_]*)?)[\s_]*(?:-+[\s_]*)?(?:(?:(?<isThemeSong>(?:NC)?(?:ED|OP)[\s_]*))?|(?<isSpecial>SP?)?|S(?<season>\d+)(?:[\s_]+(?:-+[\s_]*)?E?|E)|E?)(?<episode>\d+(?:-+\d+|\.\d+)?)(?:[vV](?<version>\d{1,2}))?(?:[\s_]*-+)?(?:[\s_]*(?:[([{](?:(?:(?:[uf]?hd|sd|bd|ld|dvd|blu[\s_-]*ray)?(?:[48]k|\d{3,4}[pi])|(?:[uf]?hd|sd)|\d{3,4}x\d{3,4}|dual[\s_-]*audio|(?:bd|dvd|ld|blu[\s_-]*ray)(?:[\s_-]*rip)?|rip|(?:hevc|x26[45])|(?:ac3|aac|flac)(?:[\s_]*[257]\.[0124])?|(?:\w{2,3}[\s_-]*)?(?:sub(?:title)?s?|dub)|(?:un)?cen(?:\.|sored)?)[\s_]*)+[)\]}]|(?:(?:(?:[uf]?hd|sd|bd|ld|dvd|blu[\s_-]*ray)?(?:[48]k|\d{3,4}[pi])|(?:[uf]?hd|sd)|\d{3,4}x\d{3,4}|dual[\s_-]*audio|(?:bd|dvd|ld|blu[\s_-]*ray)(?:[\s_-]*rip)?|rip|(?:hevc|x26[45])|(?:ac3|aac|flac)(?:[\s_]*[257]\.[0124])?|(?:\w{2,3}[\s_-]*)?(?:sub(?:title)?s?|dub)|(?:un)?cen(?:\.|sored)?)[\s_]*)+))*(?:[\s_]*\[(?<crc32>[0-9a-fA-F]{8})\])?[\s_]*\.(?<extension>[a-zA-Z0-9_\-+]+)$/i,
+    transform(pathDetails) {
+      if (pathDetails.showName) {
+        // eslint-disable-next-line no-param-reassign
+        pathDetails.showName = pathDetails.showName.replaceAll('_', ' ').replace(/\s+/g, ' ').trim();
+      }
+      return pathDetails;
+    },
   },
   // TODO: Add more rules here.
 ];
