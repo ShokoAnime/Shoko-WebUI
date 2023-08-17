c66c9da43202e1b23b677f07393651f7dc14b58b
Harshith Mohan <gharshitmohan@ymail.com>
2023-08-17 14:01:36 +0530

    Fix layout in GroupView

--- src/pages/collection/GroupView.tsx
+++ src/pages/collection/GroupView.tsx
@@ -1,5 +1,7 @@
 import React, { useMemo } from 'react';
 import { useParams } from 'react-router';
+import useMeasure from 'react-use-measure';
+import { chunk } from 'lodash';
 
 import CollectionTitle from '@/components/Collection/CollectionTitle';
 import GridViewItem from '@/components/Collection/GridViewItem';
@@ -7,20 +9,49 @@ import { useGetGroupQuery, useGetGroupSeriesQuery } from '@/core/rtkQuery/splitV
 
 import type { SeriesType } from '@/core/types/api/series';
 
+const itemWidth = 209 + 16;
+
 const GroupView = () => {
   const { groupId } = useParams();
 
   const group = useGetGroupQuery({ groupId: Number(groupId) }, { skip: !groupId });
   const series = useGetGroupSeriesQuery({ groupId }, { skip: !groupId });
-  const seriesInGroup = useMemo(() => series?.data ?? [] as Array<SeriesType>, [series?.data]);
+  const seriesInGroup = useMemo(() => series?.data ?? [] as Array<SeriesType>, [series]);
+
+  const [gridContainerRef, gridContainerBounds] = useMeasure();
+  const itemsPerRow = Math.max(1, Math.floor((gridContainerBounds.width - 40) / itemWidth));
+  const seriesRows = useMemo(() => chunk(seriesInGroup, itemsPerRow), [itemsPerRow, seriesInGroup]);
+
+  // Placeholder to solve formatting issues, same as CollectionView
+  const placeholderItems = useMemo(() => {
+    if (!seriesRows.length) return [];
+    const placeholderDeficit = itemsPerRow - seriesRows[seriesRows.length - 1].length;
+    const items = [] as React.ReactNode[];
+    for (let i = 0; i < placeholderDeficit; i += 1) {
+      items.push(<div className="w-[13.0625rem]" key={`placeholder-${i}`} />);
+    }
+    return items;
+  }, [itemsPerRow, seriesRows]);
 
   return (
     <div className="flex grow flex-col gap-y-8">
       <div className="flex items-center justify-between rounded-md border border-panel-border bg-panel-background px-8 py-10">
         <CollectionTitle count={seriesInGroup.length} filterOrGroup={group.data?.Name} />
       </div>
-      <div className="flex grow flex-wrap justify-between gap-4 border border-panel-border bg-panel-background px-7 py-8">
-        {seriesInGroup.map(item => <GridViewItem item={item} isSeries key={`series-${item.IDs.ID}`} />)}
+      <div
+        className="flex grow flex-col items-center gap-y-4 rounded-md border border-panel-border bg-panel-background p-8"
+        ref={gridContainerRef}
+      >
+        {seriesRows.map((row, idx) => (
+          <div
+            className="flex gap-x-4"
+            // eslint-disable-next-line react/no-array-index-key
+            key={`row-${idx}`}
+          >
+            {row.map(item => <GridViewItem item={item} isSeries key={`series-${item.IDs.ID}`} />)}
+            {idx === (seriesRows.length - 1) && placeholderItems}
+          </div>
+        ))}
       </div>
     </div>
   );
