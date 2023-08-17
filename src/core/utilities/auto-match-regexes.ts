7b2e6de1cb5bd7872db6968d4e232a8a55f91121
Mikal Stordal <mikalstordal@gmail.com>
2023-08-17 23:04:44 +0200

    misc: final adjustment for the episode regex

--- src/core/utilities/auto-match-regexes.ts
+++ src/core/utilities/auto-match-regexes.ts
@@ -2,13 +2,35 @@ import type { PathMatchRule } from './auto-match-logic';
 
 const PathMatchRuleSet: PathMatchRule[] = [
   {
-    name: 'general',
+    name: 'episodes',
     regex:
-      /^(?:[{[(](?<releaseGroup>[a-zA-Z0-9_-\s]+)[)}\]][\s_.]*)?(?<showName>.+?(?:[\s_.]*\((?<year>(?:19|20)\d{2})\)[\s_.]*)?)[\s_.]*(?:-+[\s_.]*)?(?:(?:(?<isThemeSong>(?:NC)?(?:ED|OP)[\s_.]*))?|(?<isSpecial>SP)?|S(?<season>\d+)(?:[\s_.]+(?:-+[\s_.]*)?E?|E)|E?)(?<episode>\d+(?:-+\d+|\.\d+)?)(?:v(?<version>\d{1,2}))?(?<episodeName>.*)\.(?<extension>[a-zA-Z0-9_\-+]+)$/i,
-    transform(pathDetails) {
-      if (pathDetails.showName && !pathDetails.showName.includes(' ')) {
+      /^(?:[{[(](?<releaseGroup>[^)}\]]+)[)}\]][\s_.]*)?(?<showName>(?:[a-z]+[\s_.]+\d+(?=[\s_.]*(?:-+[\s_.]*)[a-z]+))?.+?(?<!\d)(?:[\s_.]*\((?<year>(?:19|20)\d{2})\))?)[\s_.]*(?:-+[\s_.]*)?(?:(?:(?<isThemeSong>(?:NC)?(?:ED|OP)[\s_.]*))|(?<isSpecial>SP)|(?<isOVA>OVA)|(?<isMovie>(?:The[\s_.]+)?Movie)(?:[\s_.]+(?:[_-]+[\s_.]*)?E?|(?=E))|S(?:eason)?(?<season>\d+)(?:[\s_.]+(?:[_-]+[\s_.]*)?E?|(?=E))|)(?<!part)(?:e?(?<episode>\d+(?:-+\d+|\.5)?)(?:v(?<version>\d{1,2}))?)(?:[\s_.]*-+(?:[\s_.]+(?<episodeTitle>(?!\d)[^([{\n]*?))?)?(?:[\s_.]+(?:[\s_.]*-+)?)?(?:[\s_.]*(?:\([^)]*\)|\[[^\]]*\]|{[^}]*}|[([{]+[^)\]}\n]*[)\]}]+|(?:(?:(?:[uf]?hd|sd|bd|ld|dvd|blu[\s_.-]*ray)?(?:[48]k|\d{3,4}[pi])(?:-+hi\w*)?|(?:[uf]?hd|sd)|\d{3,4}x\d{3,4}|dual[\s_.-]*audio|(?:www|web|bd|dvd|ld|blu[\s_.-]*ray)(?:[\s_.-]*(?:rip|dl))?|dl|rip|(?:av1|hevc|h26[45])(?:-[a-z0-9]{1,3})?|(?:opus|ac3|aac|flac)(?:[\s._]*[257]\.[0124](?:[_.-]+\w{1,3})?)?|(?:\w{2,3}[\s_.-]*)?(?:sub(?:title)?s?|dub)|(?:un)?cen(?:\.|sored)?)[\s_.]*)+))*[\s_.]*\.(?:[a-zA-Z0-9_\-+]+)$/i,
+    transform(pathDetails, match) {
+      // TODO: mohan couldn't find a dprint setting to make the = go to next line. This needs to be fixed.
+      // eslint-disable-next-line operator-linebreak
+      const TrimShowNameRegex =
+        /(?![\s_.]*\((?:19|20)\d{2}\))(?:[\s_.]*(?:[([{][^)\]}\n]*[)\]}]|(?:(?:(?:[uf]?hd|sd|bd|ld|dvd|blu[\s_.-]*ray)?(?:[48]k|\d{3,4}[pi])(?:-+hi\w*)?|(?:[uf]?hd|sd)|\d{3,4}x\d{3,4}|dual[\s_.-]*audio|(?:www|web|bd|dvd|ld|blu[\s_.-]*ray)(?:[\s_.-]*(?:rip|dl))?|dl|rip|(?:av1|hevc|h26[45])(?:-[a-z0-9]{1,3})?|(?:opus|ac3|aac|flac)(?:[\s._]*[257]\.[0124](?:[_.-]+\w{1,3})?)?|(?:\w{2,3}[\s_.-]*)?(?:sub(?:title)?s?|dub)|(?:un)?cen(?:\.|sored)?)[\s_.]*)+))*[\s_.]*$/i;
+
+      // Fix up show name by removing unwanted details and fixing spaces.
+      if (pathDetails.showName) {
+        let showName = pathDetails.showName.replace(TrimShowNameRegex, '');
+
+        // Convert underscores and dots to spaces if we don't have any spaces in
+        // the show name yet.
+        if (!showName.includes(' ')) {
+          showName = showName.replace(/[_.]+/g, ' ').replace(/\s+/g, ' ').trim();
+        }
+
         // eslint-disable-next-line no-param-reassign
-        pathDetails.showName = pathDetails.showName.replace(/[_.]+/g, ' ').replace(/\s+/g, ' ').trim();
+        pathDetails.showName = showName;
+      }
+
+      // Correct movie numbering.
+      if (
+        match.groups!.isMovie && pathDetails.episodeStart === 0 && pathDetails.episodeEnd === 0
+      ) {
+        // eslint-disable-next-line no-multi-assign, no-param-reassign
+        pathDetails.episodeStart = pathDetails.episodeEnd = 1;
       }
       return pathDetails;
     },
