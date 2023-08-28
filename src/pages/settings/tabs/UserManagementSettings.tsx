bfe42a9fd6d62e351b682c9e9232a7cb85231611
Harshith Mohan <gharshitmohan@ymail.com>
2023-08-28 21:41:55 +0530

    Fix avatar edit modal

--- src/pages/settings/tabs/UserManagementSettings.tsx
+++ src/pages/settings/tabs/UserManagementSettings.tsx
@@ -302,8 +302,8 @@ function UserManagementSettings() {
           <div className="flex items-center justify-between">
             Change Avatar
             <div className="flex gap-x-2">
-              <Button
-                buttonType="secondary"
+              <label
+                htmlFor="avatar"
                 className="cursor-pointer rounded-md border border-panel-border bg-button-secondary px-3 py-2 text-xs font-semibold drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] hover:bg-button-secondary-hover"
               >
                 Pick
@@ -314,7 +314,7 @@ function UserManagementSettings() {
                   className="hidden"
                   accept="image/*"
                 />
-              </Button>
+              </label>
               {selectedUser.Avatar && (
                 <Button onClick={removeAvatar} buttonType="danger" className="px-3 py-2 text-xs font-semibold">
                   Remove
