1312a57d0a1e5b4bba9655c19258cee7938f3218
Harshith Mohan <gharshitmohan@ymail.com>
2023-09-25 00:21:37 +0530

    Remove type on `oldWebuiSettings`

--- src/core/patches.ts
+++ src/core/patches.ts
@@ -2,7 +2,7 @@ import type { WebUISettingsType } from './types/api/settings';
 
 export const webuiSettingsPatches = {
   5: (oldWebuiSettings) => {
-    const webuiSettings = oldWebuiSettings as WebUISettingsType;
+    const webuiSettings = oldWebuiSettings;
     if (oldWebuiSettings.updateChannel === 'unstable') webuiSettings.updateChannel = 'Dev';
     else webuiSettings.updateChannel = 'Stable';
     return { ...webuiSettings, settingsRevision: 5 };
