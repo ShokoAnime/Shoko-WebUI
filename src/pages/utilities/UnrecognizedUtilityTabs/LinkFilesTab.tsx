c57e75f806c50d8e1393e61e0c30d7b4fde9d4cd
Mikal Stordal <mikalstordal@gmail.com>
2023-11-14 17:47:05 +0100

    fix: don't try to refresh and discard files query before returning from the manually linking page to the unrecognized files utility. this was slowing the process down **a lot** while I was trying to mass manually link files. the files query on the unrecognised utility also might require different parameters the next time it renders so the response we got will be discarded anyways.

--- src/pages/utilities/UnrecognizedUtilityTabs/LinkFilesTab.tsx
+++ src/pages/utilities/UnrecognizedUtilityTabs/LinkFilesTab.tsx
@@ -27,11 +27,7 @@ import ItemCount from '@/components/Utilities/Unrecognized/ItemCount';
 import MenuButton from '@/components/Utilities/Unrecognized/MenuButton';
 import RangeFillModal from '@/components/Utilities/Unrecognized/RangeFillModal';
 import Title from '@/components/Utilities/Unrecognized/Title';
-import {
-  useGetFilesQuery,
-  usePostFileLinkManyMutation,
-  usePostFileLinkOneMutation,
-} from '@/core/rtkQuery/splitV3Api/fileApi';
+import { usePostFileLinkManyMutation, usePostFileLinkOneMutation } from '@/core/rtkQuery/splitV3Api/fileApi';
 import {
   useDeleteSeriesMutation,
   useGetSeriesAniDBEpisodesQuery,
@@ -237,7 +233,6 @@ function LinkFilesTab() {
     pageSize: 0,
     includeMissing: 'true',
   }, { skip: !selectedSeries.ID || selectedSeries.Type === SeriesTypeEnum.Unknown });
-  const filesQuery = useGetFilesQuery({ pageSize: 0, includeUnrecognized: 'only' });
 
   const showDataMap = useMemo(() =>
     new Map(
@@ -529,7 +524,6 @@ function LinkFilesTab() {
       }),
     ]);
 
-    await filesQuery.refetch();
     setLoading({ isLinking: false, isLinkingRunning: false, createdNewSeries: false });
     setLinks([]);
     setSelectedSeries({} as SeriesAniDBSearchResult);
