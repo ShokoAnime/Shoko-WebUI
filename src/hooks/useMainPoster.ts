11f9e0c5888ceca283783b33d54e6f71a97b6de0
Mikal Stordal <mikalstordal@gmail.com>
2023-11-14 17:27:49 +0100

    misc: fix up method signatures

--- src/hooks/useMainPoster.ts
+++ src/hooks/useMainPoster.ts
@@ -4,7 +4,7 @@ import type { CollectionGroupType } from '@/core/types/api/collection';
 import type { ImageType } from '@/core/types/api/common';
 import type { SeriesType } from '@/core/types/api/series';
 
-function useMainPoster(target: SeriesType | CollectionGroupType): ImageType | null {
+function useMainPoster(target: SeriesType | CollectionGroupType | null | undefined): ImageType | null {
   return useMemo(() => {
     const posters = target?.Images?.Posters ?? [];
     return posters.find(poster => poster.Preferred) ?? posters[0] ?? null;
