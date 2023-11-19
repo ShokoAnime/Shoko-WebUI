ec98dae3aaa763856adb1a0c383aa2361c0c93d3
Mikal Stordal <mikalstordal@gmail.com>
2023-11-19 22:23:06 +0100

    fix: move around the series info panels to align with the mock-up.

--- src/components/Collection/SeriesInfo.tsx
+++ src/components/Collection/SeriesInfo.tsx
@@ -48,57 +48,6 @@ const SeriesInfo = () => {
 
   return (
     <div className="flex w-full max-w-[31.25rem] flex-col gap-y-8">
-      <div className="flex w-full flex-row justify-around gap-y-4 rounded-md border border-panel-border bg-panel-background-transparent p-8">
-        <div className="flex flex-col items-center">
-          <div className="font-semibold ">Local</div>
-          <div className="flex flex-row gap-x-1">
-            <span>EP:</span>
-            <span>{formatThousand(series.Sizes.Local.Episodes)}</span>
-            {series.Sizes.Local.Specials !== 0 && (
-              <>
-                <span>|</span>
-                <span>SP:</span>
-                <span>{formatThousand(series.Sizes.Local.Specials)}</span>
-              </>
-            )}
-          </div>
-        </div>
-        <div className="flex flex-col items-center">
-          <div className="font-semibold ">Watched</div>
-          <div className="flex flex-row gap-x-1">
-            <span>EP:</span>
-            <span>{formatThousand(series.Sizes.Watched.Episodes)}</span>
-            {(series.Sizes.Total.Specials !== 0 && series.Sizes.Local.Specials !== 0) && (
-              <>
-                <span>|</span>
-                <span>SP:</span>
-                <span>{formatThousand(series.Sizes.Watched.Specials)}</span>
-              </>
-            )}
-          </div>
-        </div>
-
-        {(series.Sizes.Missing.Episodes !== 0 || series.Sizes.Missing.Specials !== 0) && (
-          <div className="flex flex-col items-center">
-            <div className="font-semibold ">Missing</div>
-            <div className="flex flex-row gap-x-1">
-              {series.Sizes.Missing.Episodes !== 0 && (
-                <>
-                  <span>EP:</span>
-                  <span>{formatThousand(series.Sizes.Missing.Episodes)}</span>
-                </>
-              )}
-              {series.Sizes.Missing.Episodes !== 0 && series.Sizes.Missing.Specials !== 0 && <span>|</span>}
-              {series.Sizes.Missing.Specials !== 0 && (
-                <>
-                  <span>SP:</span>
-                  <span>{formatThousand(series.Sizes.Missing.Specials)}</span>
-                </>
-              )}
-            </div>
-          </div>
-        )}
-      </div>
       <div className="flex w-full flex-col gap-y-3 rounded-md border border-panel-border bg-panel-background-transparent p-8">
         <div className="flex justify-between capitalize">
           <div className="font-semibold">Type</div>
@@ -151,6 +100,57 @@ const SeriesInfo = () => {
           <div className="font-semibold">Studio</div>
           <div>{overview?.Studios?.[0] ? overview?.Studios?.[0].Name : 'Studio Not Listed'}</div>
         </div>
+      </div>
+      <div className="flex w-full flex-row justify-around gap-y-4 rounded-md border border-panel-border bg-panel-background-transparent p-8">
+        <div className="flex flex-col items-center">
+          <div className="font-semibold ">Local</div>
+          <div className="flex flex-row gap-x-1">
+            <span>EP:</span>
+            <span>{formatThousand(series.Sizes.Local.Episodes)}</span>
+            {series.Sizes.Local.Specials !== 0 && (
+              <>
+                <span>|</span>
+                <span>SP:</span>
+                <span>{formatThousand(series.Sizes.Local.Specials)}</span>
+              </>
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col items-center">
+          <div className="font-semibold ">Watched</div>
+          <div className="flex flex-row gap-x-1">
+            <span>EP:</span>
+            <span>{formatThousand(series.Sizes.Watched.Episodes)}</span>
+            {(series.Sizes.Total.Specials !== 0 && series.Sizes.Local.Specials !== 0) && (
+              <>
+                <span>|</span>
+                <span>SP:</span>
+                <span>{formatThousand(series.Sizes.Watched.Specials)}</span>
+              </>
+            )}
+          </div>
+        </div>
+
+        {(series.Sizes.Missing.Episodes !== 0 || series.Sizes.Missing.Specials !== 0) && (
+          <div className="flex flex-col items-center">
+            <div className="font-semibold ">Missing</div>
+            <div className="flex flex-row gap-x-1">
+              {series.Sizes.Missing.Episodes !== 0 && (
+                <>
+                  <span>EP:</span>
+                  <span>{formatThousand(series.Sizes.Missing.Episodes)}</span>
+                </>
+              )}
+              {series.Sizes.Missing.Episodes !== 0 && series.Sizes.Missing.Specials !== 0 && <span>|</span>}
+              {series.Sizes.Missing.Specials !== 0 && (
+                <>
+                  <span>SP:</span>
+                  <span>{formatThousand(series.Sizes.Missing.Specials)}</span>
+                </>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
