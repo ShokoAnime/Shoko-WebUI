2a89b2c92fcd1ae41858fd642d3e63cbe6e9e759
Harshith Mohan <gharshithmohan@gmail.com>
2024-01-08 01:57:19 +0530

    SeriesWithoutFilesUtility: fix series name

--- src/pages/utilities/SeriesWithoutFilesUtility.tsx
+++ src/pages/utilities/SeriesWithoutFilesUtility.tsx
@@ -44,7 +44,7 @@ const columns: UtilityHeaderType<SeriesType>[] = [
     id: 'name',
     name: 'Name',
     className: 'line-clamp-2 grow basis-0 overflow-hidden',
-    item: series => <div title={series.Name}>series.Name</div>,
+    item: series => <div title={series.Name}>{series.Name}</div>,
   },
   {
     id: 'created',
