f62f7e0fc1957e802d8c058bf08ae34c3e305113
hidden4003 <hidden4003@users.noreply.github.com>
2024-01-16 19:46:41 +0200

    Fix sentry build crash (#790)

--- webpack.config.default.js
+++ webpack.config.default.js
@@ -5,7 +5,7 @@ const path = require('path');
 const webpack = require('webpack');
 const AssetsPlugin = require('assets-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const Version = require('./public/version.json');
@@ -162,7 +162,7 @@ if (!isDebug) {
     },
   };
   if (process.env.SENTRY_AUTH_TOKEN) {
-    config.plugins.push(new SentryWebpackPlugin({
+    config.plugins.push(sentryWebpackPlugin({
       authToken: process.env.SENTRY_AUTH_TOKEN,
       org: 'shoko-anime',
       project: 'shoko-webui',
