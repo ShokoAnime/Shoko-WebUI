80141924061909739be8e39cef84edbd71e8d9c4
Harshith Mohan <gharshitmohan@ymail.com>
2024-04-03 15:11:11 +0530

    Add ShokoRelay to plex library filter

--- src/core/react-query/plex/helpers.ts
+++ src/core/react-query/plex/helpers.ts
@@ -1,4 +1,6 @@
 import type { PlexLibraryType } from '@/core/types/api/plex';
 
 export const transformLibraries = (response: PlexLibraryType[]) =>
-  response.filter(library => library.Agent === 'com.plexapp.agents.shoko');
+  response.filter(
+    library => library.Agent === 'com.plexapp.agents.shoko' || library.Agent === 'com.plexapp.agents.shokorelay',
+  );
