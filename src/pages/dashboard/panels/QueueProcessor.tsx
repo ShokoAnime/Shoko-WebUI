52255e6e7d7364aea7742df2d1d0c8a1bae9688e
Harshith Mohan <gharshithmohan@gmail.com>
2024-04-03 23:03:16 +0530

    Show `Title` when queue item is running

--- src/pages/dashboard/panels/QueueProcessor.tsx
+++ src/pages/dashboard/panels/QueueProcessor.tsx
@@ -80,7 +80,7 @@ const QueueItem = ({ item }: { item: QueueItemType }) => (
   <div className="mr-3 flex items-center justify-between gap-x-2 rounded-md p-3 even:bg-panel-background-alt">
     <div className="flex flex-col gap-y-1 break-all">
       <span className="text-sm opacity-65">
-        {item.Type}
+        {item.IsRunning ? item.Title : item.Type}
         {item.StartTime && ` | ${dayjs(item.StartTime).format('MMMM DD YYYY, HH:mm')}`}
       </span>
       {map(item.Details, (value, key) => `${key}: ${value}`).join(', ')}
