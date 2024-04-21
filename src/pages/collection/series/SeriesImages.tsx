9df15be13517108f8cd274400598c031cb8dd41d
Jordan Fearnley <fearnlj01@gmail.com>
2024-04-21 04:41:42 +0100

    Redundant settings, begone Correct styling, hello

--- src/pages/collection/series/SeriesImages.tsx
+++ src/pages/collection/series/SeriesImages.tsx
@@ -3,17 +3,14 @@ import { useParams } from 'react-router';
 import { mdiStarCircleOutline } from '@mdi/js';
 import { Icon } from '@mdi/react';
 import cx from 'classnames';
-import { cloneDeep, get, map, split, toNumber } from 'lodash';
+import { get, map, split, toNumber } from 'lodash';
 
 import BackgroundImagePlaceholderDiv from '@/components/BackgroundImagePlaceholderDiv';
 import Button from '@/components/Input/Button';
-import Checkbox from '@/components/Input/Checkbox';
 import ShokoPanel from '@/components/Panels/ShokoPanel';
 import toast from '@/components/Toast';
 import { useChangeSeriesImageMutation } from '@/core/react-query/series/mutations';
 import { useSeriesImagesQuery } from '@/core/react-query/series/queries';
-import { usePatchSettingsMutation } from '@/core/react-query/settings/mutations';
-import { useSettingsQuery } from '@/core/react-query/settings/queries';
 
 import type { ImageType } from '@/core/types/api/common';
 
@@ -56,19 +53,15 @@ const InfoLine = ({ title, value }) => (
 );
 
 const sizeMap = {
-  Posters: 'h-[15rem] w-[10rem] 2xl:h-[21rem] 2xl:w-[13rem]',
-  Fanarts: 'h-[11.5rem] w-[22.5rem] 2xl:h-[16rem] 2xl:w-[26.55rem]',
-  Banners: 'h-[8rem] w-[46rem] 2xl:w-[41rem]',
+  Posters: 'h-[15rem] w-[10rem] 2xl:h-[21rem] 2xl:w-[13.313rem]',
+  Fanarts: 'h-[11.5rem] w-[22.5rem] 2xl:h-[16rem] 2xl:w-[28.125rem]',
+  Banners: 'h-[8rem] w-[46rem] 2xl:w-[42.938rem]',
 };
 
 const isSizeMapType = (type: string): type is keyof typeof sizeMap => type in sizeMap;
 
 const SeriesImages = () => {
   const { seriesId } = useParams();
-
-  const initialSettings = useSettingsQuery().data;
-  const [settings, setSettings] = useState(initialSettings);
-  const { isPending, mutate: patchSettings } = usePatchSettingsMutation();
 
   const [type, setType] = useState('Posters');
   const [selectedImage, setSelectedImage] = useState<ImageType>({} as ImageType);
@@ -83,49 +76,12 @@ const SeriesImages = () => {
     setSelectedImage({} as ImageType);
   };
 
-  const handleSettingsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { checked, id } = event.target;
-    const updatedSettings = cloneDeep(settings);
-    updatedSettings.WebUI_Settings.collection.image[id] = checked;
-    setSettings(updatedSettings);
-    patchSettings({ newSettings: updatedSettings });
-  };
-
-  const {
-    showRandomFanart,
-    showRandomPoster,
-  } = settings.WebUI_Settings.collection.image;
-
   if (!seriesId) return null;
   if (!isSizeMapType(type)) return null;
 
   return (
     <div className="flex w-full gap-x-6">
-      <div className="flex w-96 shrink-0 flex-col gap-y-6">
-        <ShokoPanel
-          title="Image Options"
-          className="flex w-full flex-col"
-          contentClassName="flex !flex-col gap-y-1 2xl:gap-x-6 h-full"
-          fullHeight={false}
-          transparent
-        >
-          <Checkbox
-            justify
-            label="Random Poster on Load"
-            id="showRandomPoster"
-            disabled={isPending}
-            isChecked={showRandomPoster}
-            onChange={handleSettingsChange}
-          />
-          <Checkbox
-            justify
-            label="Random Fanart on Load"
-            id="showRandomFanart"
-            disabled={isPending}
-            isChecked={showRandomFanart}
-            onChange={handleSettingsChange}
-          />
-        </ShokoPanel>
+      <div className="flex w-400 shrink-0 flex-col gap-y-6">
         <ShokoPanel
           title="Selected Image Info"
           className="flex w-full flex-col"
@@ -155,7 +111,7 @@ const SeriesImages = () => {
         </ShokoPanel>
       </div>
       <div className="flex grow flex-col gap-y-6">
-        <div className="flex items-center justify-between rounded-lg border border-panel-border bg-panel-background-transparent px-6 py-4">
+        <div className="flex h-[6.125rem] items-center justify-between rounded-lg border border-panel-border bg-panel-background-transparent p-6">
           <div className="text-xl font-semibold">
             Images |&nbsp;
             <span className="text-panel-text-important">{get(images, type, []).length}</span>
@@ -165,7 +121,7 @@ const SeriesImages = () => {
           </div>
           <Heading type={type} setType={setType} onTypeChange={resetSelectedImage} />
         </div>
-        <div className="flex flex-wrap gap-9 rounded-lg border border-panel-border bg-panel-background-transparent p-4">
+        <div className="flex flex-wrap gap-6 rounded-lg border border-panel-border bg-panel-background-transparent p-6">
           {map(get(images, type, []), (item: ImageType) => (
             <div
               onClick={() => {
