5c32c55543e6e7760a4123f320a830eb9a617081
Harshith Mohan <gharshitmohan@ymail.com>
2024-06-01 17:01:46 +0530

    Fix overflow on firefox (#929)

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -42,7 +42,7 @@ function MainPage() {
       <div className="flex grow flex-col overflow-x-clip">
         <ImportFolderModal />
         <TopNav />
-        <div className="shoko-scrollbar scroll-gutter grow overflow-y-auto py-6" ref={scrollRef}>
+        <div className="shoko-scrollbar scroll-gutter grow overflow-y-auto py-6 contain-strict" ref={scrollRef}>
           <div className="scroll-no-gutter mx-auto flex min-h-full w-full max-w-[120rem] flex-col px-6">
             <Outlet context={{ scrollRef }} />
           </div>
