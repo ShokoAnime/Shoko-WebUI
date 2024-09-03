5de2fafe2fec04cd82a00f5e722cf80960ff45dc
Harshith Mohan <26010946+harshithmohan@users.noreply.github.com>
2024-09-03 21:47:31 +0530

    Move version check to ErrorBoundary

--- src/pages/SentryErrorBoundaryWrapper.tsx
+++ src/pages/SentryErrorBoundaryWrapper.tsx
@@ -1,16 +1,36 @@
 import React, { useEffect } from 'react';
 import { Outlet } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import * as Sentry from '@sentry/react';
+import semver from 'semver';
 
 import ErrorBoundary from '@/components/ErrorBoundary';
 import { useVersionQuery } from '@/core/react-query/init/queries';
+import { getParsedSupportedServerVersion, parseServerVersion } from '@/core/util';
 
 const SentryErrorBoundaryWrapper = () => {
+  const navigate = useNavigate();
   const versionQuery = useVersionQuery();
 
   useEffect(() => {
     Sentry.setTag('server_release', versionQuery.data?.Server?.Version ?? 'Unknown');
   }, [versionQuery.data]);
+
+  useEffect(() => {
+    if (!versionQuery.data || versionQuery.data.Server.ReleaseChannel === 'Debug') return;
+
+    const serverData = versionQuery.data?.Server;
+
+    let isServerSupported = true;
+
+    const semverVersion = parseServerVersion(serverData.Version);
+    const minimumVersion = getParsedSupportedServerVersion();
+    if (semverVersion && semver.lt(semverVersion, minimumVersion)) isServerSupported = false;
+
+    if (!isServerSupported) {
+      navigate('/webui/unsupported');
+    }
+  }, [navigate, versionQuery.data]);
 
   return (
     <Sentry.ErrorBoundary
