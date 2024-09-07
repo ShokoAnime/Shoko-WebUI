9cf15c3334869474963244db701ce5fbba174df0
natyusha <985941+natyusha@users.noreply.github.com>
2024-09-07 17:49:37 -0400

    Formatting Take 2

--- src/components/Collection/CleanDescription.tsx
+++ src/components/Collection/CleanDescription.tsx
@@ -4,6 +4,7 @@ import cx from 'classnames';
 // The question marks are there because people can't spell…
 const CleanInfoLinesRegex = /\b((Modified )?Sour?ce|Note( [1-9])?|Summ?ary):(?!$)([^\r\n]+|$)/img;
 
+// eslint-disable-next-line operator-linebreak -- Because dprint and eslint can't agree otherwise. Feel free to fix it.
 const CleanMiscLinesRegex =
   /^^(\*|\u2014 (adapted|source:?|summary|translated|written)|- (translated)|~ (adapted|description|summary|translated)) ([^\r\n]+|$)/img;
 
