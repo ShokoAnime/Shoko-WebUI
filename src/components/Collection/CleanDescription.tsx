e722f7a81151830ab2c37938b846c557593e9ce7
natyusha <985941+natyusha@users.noreply.github.com>
2024-09-09 02:27:05 -0400

    Apply Suggestion: Condense CleanMiscLinesRegex

--- src/components/Collection/CleanDescription.tsx
+++ src/components/Collection/CleanDescription.tsx
@@ -6,7 +6,7 @@ const CleanInfoLinesRegex = /\b((Modified )?Sour?ce|Note( [1-9])?|Summ?ary):(?!$
 
 // eslint-disable-next-line operator-linebreak -- Because dprint and eslint can't agree otherwise. Feel free to fix it.
 const CleanMiscLinesRegex =
-  /^(\*|\u2014 (adapted|source:?|summary|translated|written)|- (translated)|~ (adapted|description|summary|translated)) ([^\r\n]+|$)/img;
+  /^(\*|[\u2014~-] (adapted|source|description|summary|translated|written):?) ([^\r\n]+|$)/img;
 
 // This accounts for an AniDB API bug since BBCode is not supposed to be there
 const CleanBBCodeRegex = /\[i\](?!"The Sasami|"Stellar|In the distant| occurred in)(.*?)\[\/i\]/isg;
@@ -14,7 +14,7 @@ const CleanExtraBBCodeRegex = /(\[i\]|\[\/i\])/ig;
 
 const CleanMultiEmptyLinesRegex = /\n{2,}/g;
 
-const CleanMultiSpacesRegex = /\s{2,}/g;
+const CleanMultiSpacesRegex = /\s{2,}/sg;
 
 // eslint-disable-next-line operator-linebreak -- Because dprint and eslint can't agree otherwise. Feel free to fix it.
 const LinkRegex =
