b0ad1d7dcd0a29774231bef9dd0100c74e68c7c3
Mikal Stordal <mikalstordal@gmail.com>
2024-09-10 00:10:36 +0200

    feat: add upload theme button to settings
    Added a new '🖋 Upload Theme' button to the 'Theme Options' section of the 'General' Settings tab in the settings page. With it you can upload JSON-only themes, CSS-only themes, and JSON-based online themes, all without having access to the file system of the server. The themes are also auto-applied in preview mode after they're successfully uploaded, so you can preview the theme right away before saving.
    
    **P.S.**: _If somebody have issues with the implementation, then be my guest changing it, as long as user experience remains the same in the end._

--- src/core/react-query/webui/mutations.ts
+++ src/core/react-query/webui/mutations.ts
@@ -2,7 +2,22 @@ import { useMutation } from '@tanstack/react-query';
 
 import { axios } from '@/core/axios';
 
+import type { WebuiTheme } from '@/core/types/api/webui';
+
 export const useUpdateWebuiMutation = () =>
   useMutation({
     mutationFn: (channel: 'Stable' | 'Dev') => axios.post('WebUI/Update', null, { params: { channel } }),
+  });
+
+export const useWebuiUploadThemeMutation = () =>
+  useMutation<WebuiTheme, undefined, { file: File, preview?: boolean }>({
+    mutationFn: ({ file, preview }) => {
+      const formData = new FormData();
+      formData.append('file', file);
+      if (preview) {
+        formData.append('preview', 'true');
+      }
+
+      return axios.post('WebUI/Theme/AddFromFile', formData);
+    },
   });
