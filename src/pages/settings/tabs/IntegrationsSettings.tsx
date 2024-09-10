c71464bf9f76b67a40f37f77c1da6f3c97616133
Harshith Mohan <26010946+harshithmohan@users.noreply.github.com>
2024-09-10 21:19:57 +0530

    Move trakt/plex settings to Integrations section, separate tmdb sub-sections

---
+++ src/pages/settings/tabs/IntegrationsSettings.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import PlexSettings from '@/components/Settings/MetadataSitesSettings/PlexSettings';
+import TraktSettings from '@/components/Settings/MetadataSitesSettings/TraktSettings';
+
+const IntegrationsSettings = () => (
+  <>
+    <div className="flex flex-col gap-y-1">
+      <div className="text-xl font-semibold">Integrations</div>
+      <div>
+        Customize the integrations that Shoko uses to scrobble your media
+      </div>
+    </div>
+
+    <div className="border-b border-panel-border" />
+
+    <TraktSettings />
+    <div className="border-b border-panel-border" />
+    <PlexSettings />
+    <div className="border-b border-panel-border" />
+  </>
+);
+
+export default IntegrationsSettings;
