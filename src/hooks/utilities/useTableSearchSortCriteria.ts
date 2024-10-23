ecbcf185db5ac799dd7e910707ac6df769a100c9
Harshith Mohan <26010946+harshithmohan@users.noreply.github.com>
2024-10-23 14:49:50 +0530

    Fix sorting & searching in file utils, fix file summary (#1119)
    * Fix sorting & searching in file utils
    
    * Fix file summary for unrecognized files in file search util

---
+++ src/hooks/utilities/useTableSearchSortCriteria.ts
@@ -0,0 +1,39 @@
+import type React from 'react';
+import { useEffect, useState } from 'react';
+import { useDebounceValue } from 'usehooks-ts';
+
+import useEventCallback from '@/hooks/useEventCallback';
+
+import type { FileSortCriteriaEnum } from '@/core/types/api/file';
+
+const useTableSearchSortCriteria = (defaultSortCriteria: FileSortCriteriaEnum) => {
+  const [sortCriteria, setSortCriteria] = useState<number>(defaultSortCriteria);
+  const [searchSortCriteria, setSearchSortCriteria] = useState<number>();
+
+  const [search, setSearch] = useState('');
+  const [debouncedSearch] = useDebounceValue(search, 250);
+
+  const updateSortCriteria = useEventCallback((newCriteria: FileSortCriteriaEnum) => {
+    if (debouncedSearch) setSearchSortCriteria(newCriteria);
+    else setSortCriteria(newCriteria);
+  });
+
+  const updateSearch = useEventCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  });
+
+  // Reset search sort criteria if search is cleared
+  useEffect(() => {
+    if (searchSortCriteria && !debouncedSearch) setSearchSortCriteria(undefined);
+  }, [debouncedSearch, searchSortCriteria]);
+
+  return {
+    search,
+    setSearch: updateSearch,
+    debouncedSearch,
+    sortCriteria: debouncedSearch ? searchSortCriteria : sortCriteria,
+    setSortCriteria: updateSortCriteria,
+  };
+};
+
+export default useTableSearchSortCriteria;
