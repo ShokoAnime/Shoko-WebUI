c5482813fefd6cb243c74d0d0c5f2afb20e5a6e3
Mikal Stordal <mikalstordal@gmail.com>
2024-11-05 19:18:56 +0100

    fix: fix match rating in client after server-side additions

--- src/components/Collection/Tmdb/MatchRating.tsx
+++ src/components/Collection/Tmdb/MatchRating.tsx
@@ -7,10 +7,14 @@ const getAbbreviation = (rating?: MatchRatingType) => {
   switch (rating) {
     case MatchRatingType.DateAndTitleMatches:
       return 'DT';
+    case MatchRatingType.DateAndTitleKindaMatches:
+      return '~DT';
     case MatchRatingType.DateMatches:
       return 'D';
     case MatchRatingType.TitleMatches:
       return 'T';
+    case MatchRatingType.TitleKindaMatches:
+      return '~T';
     case MatchRatingType.UserVerified:
       return 'UO';
     case MatchRatingType.FirstAvailable:
@@ -31,8 +35,10 @@ const MatchRating = React.memo(({ isDisabled, isOdd, rating }: Props) => (
     className={cx(
       'flex justify-center items-center rounded-md border border-panel-border w-16 text-button-primary-text',
       {
-        'bg-panel-text-important': rating === MatchRatingType.DateAndTitleMatches,
-        'bg-panel-text-warning': rating === MatchRatingType.DateMatches || rating === MatchRatingType.TitleMatches,
+        'bg-panel-text-important': rating === MatchRatingType.DateAndTitleMatches
+          || rating === MatchRatingType.TitleMatches,
+        'bg-panel-text-warning': rating === MatchRatingType.DateMatches || rating === MatchRatingType.TitleKindaMatches
+          || rating === MatchRatingType.DateAndTitleKindaMatches,
         'bg-panel-text-primary': rating === MatchRatingType.UserVerified,
         'bg-panel-text-danger': rating === MatchRatingType.FirstAvailable,
         'bg-panel-background': (!rating || rating === MatchRatingType.None) && !isOdd,
