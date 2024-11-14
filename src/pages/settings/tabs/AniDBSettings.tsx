6b9a108d22e0a5106cf1cc2d5433d044624c373a
Harshith Mohan <26010946+harshithmohan@users.noreply.github.com>
2024-11-14 23:42:36 +0530

    Remove "Sync MyList" frequency setting (#1140)

--- src/pages/settings/tabs/AniDBSettings.tsx
+++ src/pages/settings/tabs/AniDBSettings.tsx
@@ -32,7 +32,6 @@ function AniDBSettings() {
     MyList_SetUnwatched,
     MyList_SetWatched,
     MyList_StorageState,
-    MyList_UpdateFrequency,
     Notification_HandleMovedFiles,
     Notification_UpdateFrequency,
     Password,
@@ -259,16 +258,6 @@ function AniDBSettings() {
               id="anime-information"
               value={Anime_UpdateFrequency}
               onChange={event => updateSetting('AniDb', 'Anime_UpdateFrequency', event.target.value)}
-            >
-              <UpdateFrequencyValues />
-            </SelectSmall>
-          </div>
-          <div className="flex items-center justify-between">
-            <span>Sync Mylist</span>
-            <SelectSmall
-              id="sync-mylist"
-              value={MyList_UpdateFrequency}
-              onChange={event => updateSetting('AniDb', 'MyList_UpdateFrequency', event.target.value)}
             >
               <UpdateFrequencyValues />
             </SelectSmall>
