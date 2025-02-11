9038c2880b330977d70fabbbb68026d39ebf68e2
Mikal S. <7761729+revam@users.noreply.github.com>
2025-02-11 03:00:27 +0100

    Add custom tag modal (#1171)
    * feat: add custom tag modal
    
    * misc: accommodate requested changes
    
    Co-authored-by: Harshith Mohan <26010946+harshithmohan@users.noreply.github.com>
    
    [skip ci]

--- src/core/react-query/tag/types.ts
+++ src/core/react-query/tag/types.ts
@@ -2,4 +2,21 @@ import type { PaginationType } from '@/core/types/api';
 
 export type TagsRequestType = {
   excludeDescriptions?: boolean;
+  includeCount?: boolean;
 } & PaginationType;
+
+export type CreateUserTagRequestType = {
+  name: string;
+  description?: string | null;
+};
+
+export type UpdateUserTagRequestType = {
+  tagId: number;
+  name?: string | null;
+  description?: string | null;
+};
+
+export type AddRemoveUserTagRequestType = {
+  tagId: number;
+  seriesId: number;
+};
