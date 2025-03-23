bfbafa972f5108258e8cc3b06be398a2efdd85df
Mikal Stordal <mikalstordal@gmail.com>
2025-03-23 20:15:24 +0100

    chore: add more TODOs

--- src/components/Configuration/Record/FloatRecord.tsx
+++ src/components/Configuration/Record/FloatRecord.tsx
@@ -5,6 +5,7 @@ import UnableToRenderSchema from '@/components/Configuration/UnableToRenderSchem
 import type { AnySchemaProps } from '@/components/Configuration/AnySchema';
 
 function FloatRecord(props: AnySchemaProps): React.JSX.Element | null {
+  // TODO: implement this when we get need it.
   return <UnableToRenderSchema type="Record (Float)" schema={props.schema} path={props.path} />;
 }
 
