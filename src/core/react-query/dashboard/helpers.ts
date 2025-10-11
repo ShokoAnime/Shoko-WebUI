5ec9be8f6dbeef520d3c8cb33f2d859739ae553e
Harshith Mohan <26010946+harshithmohan@users.noreply.github.com>
2025-10-11 21:45:56 +0530

    dashboard summary: merge MusicVideo and Unknown into Other

--- src/core/react-query/dashboard/helpers.ts
+++ src/core/react-query/dashboard/helpers.ts
@@ -2,8 +2,10 @@ import type { DashboardSeriesSummaryType } from '@/core/types/api/dashboard';
 
 export const transformSeriesSummary = (response: DashboardSeriesSummaryType) => {
   const result = response;
-  result.Other += (result?.Special ?? 0) + (result?.None ?? 0);
+  result.Other += (result.Special ?? 0) + (result.None ?? 0) + (result.Unknown ?? 0) + (result.MusicVideo ?? 0);
   delete result.Special;
   delete result.None;
+  delete result.MusicVideo;
+  delete result.Unknown;
   return result;
 };
