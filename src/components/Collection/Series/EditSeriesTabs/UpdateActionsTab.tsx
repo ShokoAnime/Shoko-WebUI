2572111525cd7c90a8d3898d6d30443d5233947d
Harshith Mohan <26010946+harshithmohan@users.noreply.github.com>
2026-01-28 20:47:43 +0530

    Add new Trakt actions (#1269)

--- src/components/Collection/Series/EditSeriesTabs/UpdateActionsTab.tsx
+++ src/components/Collection/Series/EditSeriesTabs/UpdateActionsTab.tsx
@@ -3,22 +3,27 @@ import React from 'react';
 import Action from '@/components/Collection/Series/EditSeriesTabs/Action';
 import {
   useAutoSearchTmdbMatchMutation,
+  useGetSeriesWatchStatesFromTraktMutation,
   useRefreshSeriesAniDBInfoMutation,
   useRefreshSeriesTMDBInfoMutation,
   useSendSeriesWatchStatesToTraktMutation,
   useUpdateSeriesTMDBImagesMutation,
 } from '@/core/react-query/series/mutations';
+import { useSettingsQuery } from '@/core/react-query/settings/queries';
 
 type Props = {
   seriesId: number;
 };
 
 const UpdateActionsTab = ({ seriesId }: Props) => {
+  const { TraktTv } = useSettingsQuery().data;
+
   const { mutate: refreshAnidb } = useRefreshSeriesAniDBInfoMutation(seriesId);
   const { mutate: autoMatchTmdb } = useAutoSearchTmdbMatchMutation(seriesId);
   const { mutate: refreshTmdb } = useRefreshSeriesTMDBInfoMutation(seriesId);
   const { mutate: updateTmdbImagesMutation } = useUpdateSeriesTMDBImagesMutation(seriesId);
   const { mutate: sendWatchStatesToTrakt } = useSendSeriesWatchStatesToTraktMutation(seriesId);
+  const { mutate: getWatchStatesFromTrakt } = useGetSeriesWatchStatesFromTraktMutation(seriesId);
 
   const triggerAnidbRefresh = (force: boolean, cacheOnly: boolean) => {
     refreshAnidb({ force, cacheOnly });
@@ -60,11 +65,20 @@ const UpdateActionsTab = ({ seriesId }: Props) => {
         description="Forces a complete redownload of images from TMDB."
         onClick={updateTmdbImagesForce}
       />
-      <Action
-        name="Send Watch States to Trakt"
-        description="Sends missing episode watch states to Trakt. This does not overwrite Trakt data."
-        onClick={sendWatchStatesToTrakt}
-      />
+      {TraktTv.Enabled && TraktTv.AuthToken && (
+        <>
+          <Action
+            name="Send Watch States to Trakt"
+            description="Sends missing episode watch states to Trakt. This does not overwrite Trakt data."
+            onClick={sendWatchStatesToTrakt}
+          />
+          <Action
+            name="Get Watch States from Trakt"
+            description="Gets missing episode watch states from Trakt. This does not overwrite local data."
+            onClick={getWatchStatesFromTrakt}
+          />
+        </>
+      )}
     </div>
   );
 };
--- src/components/Dialogs/ActionsModal.tsx
+++ src/components/Dialogs/ActionsModal.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import cx from 'classnames';
+import { produce } from 'immer';
 import { map } from 'lodash';
 
 import ModalPanel from '@/components/Panels/ModalPanel';
@@ -7,8 +8,9 @@ import toast from '@/components/Toast';
 import quickActions from '@/core/quick-actions';
 import { useRunActionMutation } from '@/core/react-query/action/mutations';
 import { useInvalidatePlexTokenMutation } from '@/core/react-query/plex/mutations';
+import { useSettingsQuery } from '@/core/react-query/settings/queries';
 
-const actions = {
+const defaultActions = {
   import: {
     title: 'Import',
     data: [
@@ -29,12 +31,6 @@ const actions = {
       'get-anidb-notifications',
       'process-moved-files',
       'sync-mylist',
-    ],
-  },
-  trakt: {
-    title: 'Trakt',
-    data: [
-      'send-watch-states-trakt',
     ],
   },
   tmdb: {
@@ -69,6 +65,13 @@ const actions = {
       'validate-all-images',
     ],
   },
+  trakt: {
+    title: 'Trakt',
+    data: [
+      'send-watch-states-trakt',
+      'get-watch-states-trakt',
+    ],
+  },
   plex: {
     title: 'Plex',
     data: [
@@ -76,9 +79,7 @@ const actions = {
       'plex-force-unlink',
     ],
   },
-};
-
-const isActionTab = (type: string): type is keyof typeof actions => type in actions;
+} as Record<string, { title: string, data: string[] }>;
 
 type Props = {
   show: boolean;
@@ -123,7 +124,12 @@ const Action = ({ actionKey, length }: { actionKey: string, length: number }) =>
 };
 
 const ActionsModal = ({ onClose, show }: Props) => {
+  const { TraktTv } = useSettingsQuery().data;
   const [activeTab, setActiveTab] = useState('import');
+
+  const actions = produce(defaultActions, (draftState) => {
+    if (!TraktTv.Enabled || !TraktTv.AuthToken) delete draftState.trakt;
+  });
 
   return (
     <ModalPanel
@@ -154,10 +160,9 @@ const ActionsModal = ({ onClose, show }: Props) => {
         <div className="border-r border-panel-border" />
         <div className="flex grow">
           <div className="scroll-gutter flex grow flex-col gap-y-4 overflow-y-auto">
-            {isActionTab(activeTab)
-              && actions[activeTab].data.map((key: string) => (
-                <Action actionKey={key} key={key} length={actions[activeTab].data.length} />
-              ))}
+            {actions[activeTab].data.map((key: string) => (
+              <Action actionKey={key} key={key} length={actions[activeTab].data.length} />
+            ))}
           </div>
         </div>
       </div>
