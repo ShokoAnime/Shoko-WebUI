16f4c7deba106f56c4a9bdb11434b043d132e86c
Harshith Mohan <26010946+harshithmohan@users.noreply.github.com>
2026-04-01 01:09:29 +1300

    Fix file search, remove usages of lodash get (#1307)

--- src/pages/utilities/FileSearch.tsx
+++ src/pages/utilities/FileSearch.tsx
@@ -17,7 +17,7 @@ import {
 } from '@mdi/js';
 import Icon from '@mdi/react';
 import cx from 'classnames';
-import { forEach, get, reverse } from 'lodash';
+import { forEach, reverse } from 'lodash';
 import prettyBytes from 'pretty-bytes';
 
 import DeleteFilesModal from '@/components/Dialogs/DeleteFilesModal';
@@ -243,14 +243,14 @@ const FileDetails = React.memo(({ fileId }: { fileId: number }) => {
     },
   );
 
-  const seriesId: number = get(file, 'SeriesIDs[0].SeriesID.ID', 0);
+  const seriesId = file?.SeriesIDs?.[0]?.SeriesID.ID ?? 0;
   const { data: seriesInfo, isFetching: seriesQueryIsPending } = useSeriesQuery(
     seriesId,
     {},
     !!seriesId,
   );
 
-  const episodeId: number = get(file, 'SeriesIDs[0].EpisodeIDs[0].ID', 0);
+  const episodeId = file?.SeriesIDs?.[0]?.EpisodeIDs?.[0]?.AniDB ?? 0;
   const { data: episodeInfo, isFetching: episodeQueryIsPending } = useEpisodeAniDBQuery(
     episodeId,
     !!episodeId,
