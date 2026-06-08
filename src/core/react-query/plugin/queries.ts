90b36fd26fd4960674321ac34211121e7eccb74c
Harshith Mohan <26010946+harshithmohan@users.noreply.github.com>
2026-06-08 19:39:55 +0530

    Change ManagedFolders stale time to ~infinite

--- src/core/react-query/plugin/queries.ts
+++ src/core/react-query/plugin/queries.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { axios } from '@/core/axios';
-import { STALE_TIME } from '@/core/util';
+import { INFINITE_STALE_TIME } from '@/core/util';
 
 import type { PluginListFilterType } from '@/core/react-query/plugin/types';
 import type { PluginInfoType, PluginPageType, SharedPluginPageType } from '@/core/types/api/plugin';
@@ -17,13 +17,13 @@ export const usePluginPagesQuery = () =>
   useQuery<SharedPluginPageType[]>({
     queryKey: ['plugin', 'pages'],
     queryFn: () => axios.get('Plugin/Pages'),
-    staleTime: STALE_TIME,
+    staleTime: INFINITE_STALE_TIME,
   });
 
 export const usePluginPagesForPluginQuery = (pluginId: string, enabled = true) =>
   useQuery<PluginPageType[]>({
     queryKey: ['plugin', 'pages', pluginId],
     queryFn: () => axios.get(`Plugin/${pluginId}/Pages`),
-    staleTime: STALE_TIME,
+    staleTime: INFINITE_STALE_TIME,
     enabled,
   });
